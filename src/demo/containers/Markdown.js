b4a0784efdb94a439a16355b8f0b413c7706f68e
Michael Lee <michael@Michaels-MacBook-Pro-2.local>
2018-07-19 14:35:18 -0700

    added logic for math rendering, still not functioning

--- src/demo/containers/Markdown.js
+++ src/demo/containers/Markdown.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {init_markdown_it} from 'markdown-it-synapse'
+let markdownitSynapse = require('markdown-it-synapse')
 let markdownSubAlt = require('markdown-it-sub-alt');
 let markdownCenterText = require('markdown-it-center-text')
 let markdownSynapseHeading = require('markdown-it-synapse-heading')
@@ -10,23 +10,23 @@ let markdownEmpahsisAlt = require('markdown-it-emphasis-alt')
 let markdownInlineComments = require('markdown-it-inline-comments')
 let markdownBr = require('markdown-it-br')
 let sanitizeHtml = require('sanitize-html');
-// let synapseMath = require('markdown-it-synapse-math') TODO: make this work
+let synapseMath = require('markdown-it-synapse-math')
 
 /*  
     Basic vanilla Markdownit functionality
 */
-class Markdown extends React.Component {
+class Markdown extends React.Component {    
 
     constructor (props) {
         super(props)
         this.state = {
             md: require('markdown-it')({html: true}),
-            text: ''
+            text: 'Pythagoran theorem is $$/(a^2 + b^2 = c^2/)$$ will render to:'
         }
-        this.createMarkup = this.createMarkup.bind(this)
-        this.createMarkupWithParam = this.createMarkupWithParam.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.updateDisplayText = this.updateDisplayText.bind(this)
+        this.processMathJax = this.processMathJax.bind(this)
+        this.createMarkup = this.createMarkup.bind(this)
     }
 
      updateDisplayText() {
@@ -47,26 +47,45 @@ class Markdown extends React.Component {
             )
     }
 
-    createMarkup() {
-        return {__html: sanitizeHtml(this.state.md.render(this.state.text, 'my-unique-div-id'), 
-                {   
-                    allowedTags: [ 'h1', 'h2', 'p', 'b', 'i', 'em', 'strong', 'a' ],
-                    allowedAttributes: {
-                        'a': [ 'href' ]
-                    }
-                }
-            )};
-    }
-
-    createMarkupWithParam(text) {
-        return {__html: sanitizeHtml(this.state.md.render(text),
+    createMarkup(text) {
+        let initText = this.state.md.render(text) 
+        let withMathText = this.processMathJax(initText)
+        let cleanText = sanitizeHtml(withMathText, 
             {   
-                allowedTags: [ 'h1', 'h2', 'p', 'b', 'i', 'em', 'strong', 'a' ],
+                allowedTags: [ 'h1', 'h2', 'p', 'b', 'i', 'em', 'strong', 'a' ,'id'],
                 allowedAttributes: {
-                    'a': [ 'href' ]
+                    'a': [ 'href' ],
+                    'span': ['id']
                 }
             }
-        )};
+        )
+        return {__html: cleanText}
+    }
+
+    processMathJax(text) {
+        window.MathJax.Hub.Queue(["Typeset",window.MathJax.Hub, "mathjax-10"]);
+
+        /**
+         * Find all math identified elements
+         *  of the form <dom element id=""> text </dom element> 
+         * 
+         */
+
+        let start = 'mathjax-'
+        let index = 10
+        let curElement = start + String(index)
+        let curElementId = start + String(index)
+        while (true) {
+            curElement = document.getElementById(curElementId)
+            if (!curElement) {
+                break
+            }
+            window.MathJax.Hub.Queue(curElement)
+            index +=1
+            curElementId = start + String(index)
+        }
+
+        return text
     }
 
     handleChange(event) {
@@ -78,24 +97,27 @@ class Markdown extends React.Component {
     } 
 
     componentDidMount() {
-        init_markdown_it(
+        markdownitSynapse.init_markdown_it(
             this.state.md, markdownSubAlt, markdownEmpahsisAlt,
             markdownCenterText, markdownSynapseHeading, markdownSynapseTable,
             markdownStrikethrough, markdownContainer, markdownEmpahsisAlt,
             markdownInlineComments, markdownBr
         )
-        this.updateDisplayText()
+        this.setState({
+            md: this.state.md.use(markdownitSynapse, '0').use(synapseMath, '0')
+        })
     }
-      
+
     render() {
+ 
         return (
             <div className="container border mt-5 pt-3">
                 <div className="row">
-                    <p className="p-2 text-center" dangerouslySetInnerHTML={this.createMarkupWithParam('# Markdown it demo!')}/>
+                    <p className="p-2 text-center" dangerouslySetInnerHTML={this.createMarkup('# Markdown it demo!')}/>
                 </div>
                 <div className="row">
                     <textarea rows={5} value={this.state.text} onChange={this.handleChange} className="col-6 border"> </textarea>
-                    <div className="col-6" dangerouslySetInnerHTML={this.createMarkup()} />
+                    <div className="col-6 challenge__description" ref={1} dangerouslySetInnerHTML={this.createMarkup(this.state.text)} />
                 </div>
             </div>
         )
