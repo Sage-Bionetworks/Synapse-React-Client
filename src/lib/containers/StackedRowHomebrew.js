5f5d38c567d163be227fec30179357b68cd7faf0
Michael Lee <michael@Michaels-MacBook-Pro-2.local>
2018-09-19 15:32:08 -0700

    added basic responsive sizing to svgs, added caret, clickable buttons to navigate stacked chart

--- src/lib/containers/StackedRowHomebrew.js
+++ src/lib/containers/StackedRowHomebrew.js
@@ -1,5 +1,7 @@
 import React from 'react'
 const uuidv4 = require('uuid/v4');
+const LEFT_CLICK = "left click"
+const RIGHT_CLICK = "right click"
 
 /**
  * Make a simple stacked bar char
@@ -13,9 +15,12 @@ export default class StackedRowHomebrew extends React.Component {
         super()
         this.handleHover = this.handleHover.bind(this)
         this.handleExit = this.handleExit.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+        this.handleArrowClick = this.handleArrowClick.bind(this)
         // the text currently under the cursor
         this.state = {
-            hoverText: ""
+            hoverText: "",
+            selectedFacets: {}
         }
     }
 
@@ -28,9 +33,9 @@ export default class StackedRowHomebrew extends React.Component {
         // add box shadow
         event.target.style.boxShadow = "25px 20px"
          // careful to avoid an infinite loop
-        if (this.state.hoverText !== incomingText) {
-            this.setState({hoverText: incomingText})
-        }
+        // if (this.state.hoverText !== incomingText) {
+        //     this.setState({hoverText: incomingText})
+        // }
     }
 
     /**
@@ -42,10 +47,47 @@ export default class StackedRowHomebrew extends React.Component {
     handleExit (event) {
         // remove box shadow
         event.target.style.boxShadow = ""
-         // careful to avoid an infinite loop
-        if (this.state.hoverText !== "") {
-            this.setState({hoverText: ""})
-        }
+    }
+
+    /**
+     * Handle column click event
+     */
+    handleClick = (dict) => (event) => {
+        // https://medium.freecodecamp.org/reactjs-pass-parameters-to-event-handlers-ca1f5c422b9
+        this.setState({hoverText: dict.value + " " + dict.count})
+    }
+
+    /**
+     * Update the state with selected facets and call props to update data
+     *
+     * @param {*} selectedFacets
+     * @memberof Facets
+     */
+    updateStateAndMakeQuery(selectedFacets) {
+        this.setState({ selectedFacets });
+        // have to reformat the selected facets to format for the api call
+        let selectedFacetsFormatted = Object.keys(selectedFacets).map(
+            key => {
+                return selectedFacets[key]
+            }
+        )
+        let queryRequest = this.props.getLastQueryRequest();
+        queryRequest.query.selectedFacets = selectedFacetsFormatted;
+        this.props.executeQueryRequest(queryRequest);
+    }
+
+    handleArrowClick = (direction) => (event) => {
+        console.log("clicked ", direction)
+    }
+
+    resize = () => this.forceUpdate()
+
+    componentDidMount() {
+      window.addEventListener('resize', this.resize)
+    }
+    
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.resize)
     }
 
     /**
@@ -66,7 +108,7 @@ export default class StackedRowHomebrew extends React.Component {
                     item.facetValues.forEach(
                         facetValue => {
                             if (item.columnName === "parentId") {
-                                x_data.push(facetValue.count)
+                                x_data.push({columnName: item.columnName , ...facetValue})
                             }
                         }
                     )
@@ -74,13 +116,19 @@ export default class StackedRowHomebrew extends React.Component {
             }
         )
         // sort the data so that the largest bars are at the front
-        x_data.sort((a,b) => {return b - a})
-        let total = x_data.reduce((a,b) => {return a + b})
+        x_data.sort((a,b) => {return b.count - a.count})
+        let total = 0
+        // sum up the counts of data
+        for (let key in x_data) { if (x_data.hasOwnProperty(key)) { total += x_data[key].count } }
         let colors = ['#dddddd','#bbbbbb','#989898','#787878','#595959','#3c3c3c','#222222'].reverse()
         return (<div style={{marginBottom:"50px"}} className="container">
-            <p> <strong> {total} </strong> files shown by {this.props.showBy} </p>
+            <div> <span><strong> {total} </strong> files shown by {this.props.showBy}</span>
+            <button className="btn btn-default" type="button" onClick={this.handleArrowClick(RIGHT_CLICK)} style={{float:"right"}}> <i className="fas fa-angle-right"></i> </button>
+            <button className="btn btn-default" type="button" onClick={this.handleArrowClick(LEFT_CLICK)} style={{float:"right"}}> <i className="fas fa-angle-left"></i> </button>
+            </div>
+            <div className="container">
              {x_data.map(
-                (count, index) => {
+                (obj, index) => {
                     let rectStyle = {
                         margin: '0px',
                         fill: `${colors[index]}`,
@@ -88,24 +136,27 @@ export default class StackedRowHomebrew extends React.Component {
                         boxShadow: "20px 20px"
                     }
                     let height = 50
-                    let width = (count / total) * 800
-                    
+                    let width = (obj.count / total) * (window.innerWidth/1.5)
+                    console.log("inner width ", width)
                     return (
                             // each svg represents one of the bars
                             // will need to change this to be responsive
                             <svg height={height} width={width} key={uuidv4()}
-                                onMouseEnter={this.handleHover(count)}
+                                onMouseEnter={this.handleHover(obj.count)}
+                                onClick={this.handleClick(obj)}
                                 onMouseLeave={this.handleExit}
                                 >
                                 <rect 
                                 height={height} width={width} style={rectStyle}>
                                 </rect>
                                 {/* display the count of this bar chart's frequency */}
-                                <text font="bold sans-serif" fill="white" x={width / 2} y={height/2}> {count}
+                                <text font="bold sans-serif" fill="white" x={width / 2} y={height/2}> {obj.count}
                                 </text>
                             </svg>)
-            }
-        )}
+                }
+            )}
+        </div>
+        <div> <i className="fas fa-caret-down"></i>  </div>
         <p> {this.props.showBy} {this.state.hoverText} </p>
          </div>)
     }
