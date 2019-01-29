43ab1dc319a6adb82bf18d7b43ea4c4b74e6b509
Michael Lee <michael.leem42@gmail.com>
2019-01-29 12:58:43 -0800

    exposed assets, moved login to be exposed, allowed underscored var names

--- src/lib/assets/ButtonContent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default ({ children, icon }) => (
-  <span style={{ paddingRight: 10, fontWeight: 500, paddingLeft: icon ? 0 : 10, paddingTop: 10, paddingBottom: 10 }}>{children}</span>
-)
