22d8ef0b7efcffa5e9211a8801aac2e5b2b2cd4b
Michael Lee <michael.leem42@gmail.com>
2019-03-22 12:33:21 -0700

    updated bundle

--- rollup.config.js
+++ rollup.config.js
@@ -69,7 +69,7 @@ export default {
 		// Common js is used to handle the import of older javascript modules not using es6 
 		commonjs(),
 		// minify the bundle with uglify
-		uglify()
+		uglify({ sourcemap: true })
 	],
 	output: {
 		globals: {
