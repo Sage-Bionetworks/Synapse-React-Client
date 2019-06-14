a46747c86005d08c7b2a9b97ce47fe7ab2e83564
Michael Lee <michael.leem42@gmail.com>
2019-06-14 10:38:49 -0700

    cleaned up headercard component

--- src/lib/containers/HeaderCard.tsx
+++ src/lib/containers/HeaderCard.tsx
@@ -1,13 +1,5 @@
 import * as React from 'react'
 
-export type HeaderCardSchema = {
-  type: string
-  title: string
-  subTitle?: string
-  description: string
-  icon: string
-}
-
 export type IconOptions = {
   [index: string]: string
 }
@@ -21,57 +13,44 @@ export type HeaderCardProps = {
   icon: string
 }
 
-export type HeaderCardState = {
-  showMoreDescription: boolean
-}
-
-export default class HeaderCard extends React.Component<HeaderCardProps, HeaderCardState> {
-
-  constructor(props: HeaderCardProps) {
-    super(props)
-    this.state = {
-      showMoreDescription: false,
-    }
+const HeaderCard:React.FunctionComponent<HeaderCardProps> = ({
+  type,
+  title,
+  subTitle,
+  description,
+  iconOptions,
+  icon,
+  backgroundColor
+}) => {
+  const style: React.CSSProperties = {
+    background: backgroundColor,
   }
-
-  render() {
-    const {
-      type,
-      title,
-      subTitle,
-      description,
-      iconOptions,
-      icon,
-      backgroundColor
-    } = this.props
-    const style: React.CSSProperties = {
-      background: backgroundColor,
-    }
-    return (
-      <div
-        style={style}
-        className={'SRC-portalCardHeader'}
-      >
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-1 iconContainer">
-              <img src={iconOptions![icon]} className="iconImg"/>
+  return (
+    <div
+      style={style}
+      className={'SRC-portalCardHeader'}
+    >
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-1 iconContainer">
+            <img src={iconOptions![icon]} className="iconImg"/>
+          </div>
+          <div className="SRC-cardContent col-md-10">
+            <div className="SRC-type">{type}</div>
+            <div className="SRC-title">
+              <h3 className="SRC-boldText SRC-blackText" style={{ margin: 'none' }}>
+                {title}
+              </h3>
             </div>
-            <div className="SRC-cardContent col-md-10">
-              <div className="SRC-type">{type}</div>
-              <div className="SRC-title">
-                <h3 className="SRC-boldText SRC-blackText" style={{ margin: 'none' }}>
-                  {title}
-                </h3>
-              </div>
-              {subTitle && <div className="SRC-author"> {subTitle} </div>}
-              <span className="SRC-font-size-base">
-                {description}
-              </span>
-            </div>
+            {subTitle && <div className="SRC-author"> {subTitle} </div>}
+            <span className="SRC-font-size-base">
+              {description}
+            </span>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default HeaderCard
