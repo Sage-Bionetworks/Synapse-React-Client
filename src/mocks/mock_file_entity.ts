78caf9d857a659099a24d371e569e6b6f8462921
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-07-03 18:12:02 -0700

    add missing mock

---
+++ src/mocks/mock_file_entity.ts
@@ -0,0 +1,8 @@
+import { FileEntity } from '../lib/utils/jsonResponses/FileEntity'
+
+export const mockFileEntity: FileEntity = {
+  parentId: "syn12034",
+  dataFileHandleId : "123332",
+  name: "my file name",
+  concreteType: "org.sagebionetworks.repo.model.FileEntity"
+}
