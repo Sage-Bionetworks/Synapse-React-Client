e6dcaf09fcfe3c06f8e25a445683aaecada47ab6
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-07-08 10:26:18 -0700

    fix build

--- src/__tests__/lib/containers/EntityForm.test.tsx
+++ src/__tests__/lib/containers/EntityForm.test.tsx
@@ -4,7 +4,7 @@ import formschemaJson from '../../../mocks/formschema.json'
 import EntityForm, { EntityFormProps } from '../../../lib/containers/EntityForm'
 import { mockUserProfileData } from '../../../mocks/mock_user_profile'
 import { mockFileEntity } from '../../../mocks/mock_file_entity'
-import Form from 'react-jsonschema-form';
+import reactJsonschemaForm from 'react-jsonschema-form'
 
 const createShallowComponent = (props: EntityFormProps) => {
   const wrapper = mount(
@@ -21,15 +21,15 @@ describe('it basic tests', () => {
   const SynapseClient = require('../../../lib/utils/SynapseClient')
   SynapseClient.getFileEntityContent = jest.fn(() => Promise.resolve(formschemaJson))
   SynapseClient.getUserProfile = jest.fn(() => Promise.resolve(mockUserProfileData))
-  const targetFolderId = "syn9988882982"
+  const targetFolderId = 'syn9988882982'
   SynapseClient.lookupChildEntity = jest.fn(() => Promise.resolve(targetFolderId))
   SynapseClient.getEntity = jest.fn(() => Promise.resolve(mockFileEntity))
-  const token: string = "123444"
-  const parentContainerId: string = "syn20355732"
-  const formSchemaEntityId: string = "syn20184776"
-  const formUiSchemaEntityId: string = "syn20184771"
+  const token: string = '123444'
+  const parentContainerId: string = 'syn20355732'
+  const formSchemaEntityId: string = 'syn20184776'
+  const formUiSchemaEntityId: string = 'syn20184771'
   const initFormData: boolean = false
-  const synIdCallback = jest.fn();
+  const synIdCallback = jest.fn()
   const props: EntityFormProps = {
     parentContainerId,
     token,
@@ -38,7 +38,6 @@ describe('it basic tests', () => {
     initFormData,
     synIdCallback
   }
-  
   it('renders without crashing', () => {
     const { wrapper } = createShallowComponent(props)
     expect(wrapper).toBeDefined()
@@ -46,6 +45,6 @@ describe('it basic tests', () => {
 
   it('renders Form', () => {
     const { wrapper } = createShallowComponent(props)
-    expect(wrapper.find(Form)).toBeDefined()
+    expect(wrapper.find(reactJsonschemaForm)).toBeDefined()
   })
 })
