d9a7fe448388fd1ccba71bfca0697f6608f606ad
Michael Lee <michael.leem42@gmail.com>
2019-07-09 15:56:59 -0700

    cleanup

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,21 +1,3 @@
-declare var katex: any
-
-declare var markdownitSynapse: any
-declare var markdownit: any
-declare var markdownitSub: any
-declare var markdownitsSup: any
-declare var markdownitCentertext: any
-declare var markdownitSynapseHeading: any
-declare var markdownitSynapseTable: any
-declare var markdownitStrikethroughAlt: any
-declare var markdownitContainer: any
-declare var markdownitEmphasisAlt: any
-declare var markdownitInlineComments: any
-declare var markdownitBr: any
-declare var sanitizeHtml: any
-
-declare var markdownitMath: any
-
 // TODO: Figure out a way to get the actual types for plotly if need be
 // https://stackoverflow.com/questions/39084438/how-to-import-plotly-js-into-typescript
 declare module 'plotly.js-basic-dist'
