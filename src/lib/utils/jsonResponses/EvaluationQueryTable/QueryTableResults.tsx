039eb8db5d681c7b5ef59a834d7ffe294cd3970c
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-09-09 15:34:22 -0700

    PORTALS-674: if we go forward with an evaluation queue, then the nomination tool will need a way to run an evaluation query.

---
+++ src/lib/utils/jsonResponses/EvaluationQueryTable/QueryTableResults.tsx
@@ -0,0 +1,11 @@
+// https://docs.synapse.org/rest/org/sagebionetworks/repo/model/query/Row.html
+export type Row = {
+  values: string []
+}
+
+// https://docs.synapse.org/rest/org/sagebionetworks/repo/model/query/QueryTableResults.html
+export type QueryTableResults = {
+  headers: string []
+  rows: Row []
+  totalNumberOfResults: number
+}
