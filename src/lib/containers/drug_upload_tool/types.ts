6c93b3ca28d7ca1f4e6e6a18c405343b20585327
Alina Gendel <alina.gendel@sagebase.org>
2019-09-10 11:40:35 -0700

    fix(CNT): ts step type update

--- src/lib/containers/drug_upload_tool/types.ts
+++ src/lib/containers/drug_upload_tool/types.ts
@@ -1,50 +1,47 @@
-
 export enum StepStateEnum {
-    TODO,
-    PROGRESS,
-    COMPLETED,
-    EXCLUDED,
-    ERROR
-  }
-  
+  TODO,
+  PROGRESS,
+  COMPLETED,
+  EXCLUDED,
+  ERROR
+}
 
 export type Step = {
-    id: string;
-    title: string;
-    order: number;
-    state: StepStateEnum,
-    rules: any[],
-    default: string,
-    disabled?: boolean,
-    final?: boolean,
-    description?: string,
-    copy? : string,
-    static?: boolean,
-    child?: boolean,
-    children?: string[]
-  }
+  id: string;
+  title: string;
+  order: number;
+  state: StepStateEnum;
+  inProgress: boolean;
+  rules: any[];
+  default: string;
+  excluded?: boolean;
+  final?: boolean;
+  description?: string;
+  copy?: string;
+  static?: boolean;
+  child?: boolean;
+  children?: string[];
+};
 
-  export enum NavActionEnum {
-    PREVIOUS,
-    NEXT,
-    NAV,
-    SAVE,
-    SUBMIT,
-    NONE,
-  }
+export enum NavActionEnum {
+  PREVIOUS,
+  NEXT,
+  NAV,
+  SAVE,
+  SUBMIT,
+  NONE
+}
 
-  export enum StatusEnum {
-    PROGRESS,
-    ERROR,
-    SUCCESS,
-    PROGRESS2,
-    ERROR_CRITICAL,
-    }
-
+export enum StatusEnum {
+  PROGRESS,
+  ERROR,
+  SUCCESS,
+  PROGRESS2,
+  ERROR_CRITICAL
+}
 
 export interface SummaryFormat {
-  screen: Step,
-  label: string,
-  value: string,
+  screen: Step;
+  label: string;
+  value: string;
 }
-  
