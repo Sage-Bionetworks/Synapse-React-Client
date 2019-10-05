8279110d9b576f203a81a71d598b5e6caf4502e5
jay-hodgson <jay.hodgson@sagebase.org>
2019-10-05 13:54:12 -0700

    from code review

--- src/lib/utils/SynapseClient.ts
+++ src/lib/utils/SynapseClient.ts
@@ -1280,21 +1280,20 @@ export const getProjectStatistics = (
 }
 
 const setCookie = (name: string, value: string | undefined, days: number) => {
-  if (value) {
-    let expires = ''
-    if (days) {
-      const date = new Date()
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
-      expires = `; expires=${date.toUTCString()}`
-    }
-    document.cookie = `${name}=${value || ''}${expires}; path=/`
+  if (!value) {return}
+  let expires = ''
+  if (days) {
+    const date = new Date()
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+    expires = `; expires=${date.toUTCString()}`
   }
+  document.cookie = `${name}=${value || ''}${expires}; path=/`
 }
 const getCookie = (name: string) => {
-  let nameEQ = `${name}=`
+  const nameEQ = `${name}=`
   const ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
     while (c.charAt(0) == ' ') c = c.substring(1, c.length);
     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
