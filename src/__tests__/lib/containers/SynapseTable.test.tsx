bced88673babfc591a1af798fbdca30e9f2c96a2
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-10-15 11:43:38 -0700

    note cases that require facet aliases

--- src/__tests__/lib/containers/SynapseTable.test.tsx
+++ src/__tests__/lib/containers/SynapseTable.test.tsx
@@ -103,6 +103,10 @@ describe('basic functionality', () => {
       expect(unCamelCase('DNA')).toEqual('DNA')
       expect(unCamelCase('AnotherCase')).toEqual('Another Case')
       expect(unCamelCase('silly ColumnName Test (ms)')).toEqual('Silly Column Name Test (ms)')
+      // these cases do not work as expected, and still need facet aliases
+      // expect(unCamelCase('pH')).toEqual('pH') // actual 'P H'
+      // expect(unCamelCase('nf1Genotype/nf2Genotype')).toEqual('NF1 Genotype/NF2 Genotype') // actual 'Nf1Genotype/nf2Genotype'
+      // expect(unCamelCase('cBioFileFormat')).toEqual('cBio File Format') // actual 'C Bio File Format'
     })
   })
 
