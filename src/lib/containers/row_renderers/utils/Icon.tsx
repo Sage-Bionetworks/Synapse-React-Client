096ff873b47178da0c1a137d453bde304cc67065
Michael Lee <michael.leem42@gmail.com>
2019-10-28 10:58:41 -0700

    new icons added

--- src/lib/containers/row_renderers/utils/Icon.tsx
+++ src/lib/containers/row_renderers/utils/Icon.tsx
@@ -10,19 +10,22 @@ import {
   EXPERIMENTAL,
   COMPUTATIONAL,
   CLINICAL,
-  PROJECT
+  PROJECT,
 } from '../../../utils/SynapseConstants'
 
-import PublicationSvg from '../../../assets/icons/Publication.svg'
 import Data2Svg from '../../../assets/icons/Data2.svg'
 import studyActiveSvg from '../../../assets/icons/study-active.svg'
-import studySvg from '../../../assets/icons/study.svg'
 import studyCompleteSvg from '../../../assets/icons/study-complete.svg'
 import DNA_TwoSvg from '../../../assets/icons/DNA_Two.svg'
-import ToolClinicalSvg from '../../../assets/icons/ToolClinical.svg'
-import ToolComputationalSvg from '../../../assets/icons/ToolComputational.svg'
-import ToolExperimentalSvg from '../../../assets/icons/ToolExperimental.svg'
-import projectSvg from '../../../assets/icons/project.svg'
+
+import {
+  Project,
+  Publication,
+  Study,
+  ToolClinical,
+  ToolComputational,
+  ToolExperimental,
+} from '../../../assets/new_icons'
 
 import { KeyValue } from '../../../../lib/utils/modules/sqlFunctions'
 
@@ -33,25 +36,34 @@ type IconProps = {
 }
 
 const defaultIcons = {
-  [PUBLICATION]: PublicationSvg,
   [DATASET]: Data2Svg,
   [FUNDER]: Data2Svg,
+  [TOOL]: DNA_TwoSvg,
   [STUDY_ACTIVE]: studyActiveSvg,
   [STUDY_COMPLETE]: studyCompleteSvg,
-  [STUDY]: studySvg,
-  [TOOL]: DNA_TwoSvg,
-  [EXPERIMENTAL]: ToolExperimentalSvg,
-  [COMPUTATIONAL]: ToolComputationalSvg,
-  [CLINICAL]: ToolClinicalSvg,
-  [PROJECT]: projectSvg
+  // new icons
+  [PUBLICATION]: Publication,
+  [STUDY]: Study,
+  [EXPERIMENTAL]: ToolExperimental,
+  [COMPUTATIONAL]: ToolComputational,
+  [CLINICAL]: ToolClinical,
+  [PROJECT]: Project,
 }
-const Icon: React.FunctionComponent<IconProps> = ({ type, value = '', iconOptions }) => {
+const Icon: React.FunctionComponent<IconProps> = ({
+  type,
+  value = '',
+  iconOptions,
+}) => {
   const iconSet = { ...defaultIcons, ...iconOptions }
   // see if the value has a corresponding icon, e.g. 'Active' in a studies table
   // or if the type of card has a corresponding icon, e.g. 'Publication'
-  const icon = iconSet[value] || iconSet[type]
+  const Icon = iconSet[value] || iconSet[type]
   // TODO: get rid of dataset icon class, none of the icons should be special cased
-  const datasetIconClass = (value === DATASET) || (type === DATASET) ? 'SRC-datasetIcon' : ''
-  return (<img className={`iconImg  ${datasetIconClass}`} src={icon} />)
+  const datasetIconClass =
+    value === DATASET || type === DATASET ? 'SRC-datasetIcon' : ''
+  if (typeof Icon == 'function') {
+    return <Icon />
+  }
+  return <img src={Icon} className={`iconImg ${datasetIconClass}`} />
 }
 export default Icon
