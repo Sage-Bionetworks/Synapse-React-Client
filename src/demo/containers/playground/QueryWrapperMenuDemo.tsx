207769b974134c9dccdf6410097d993ed1d25be5
Michael Lee <michael.leem42@gmail.com>
2019-10-31 13:02:02 -0700

    added ability to render markdown in table, added test

--- src/demo/containers/playground/QueryWrapperMenuDemo.tsx
+++ src/demo/containers/playground/QueryWrapperMenuDemo.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
-import QueryWrapperMenu, { MenuConfig, QueryWrapperMenuProps } from '../../../lib/containers/QueryWrapperMenu'
+import QueryWrapperMenu, {
+  MenuConfig,
+  QueryWrapperMenuProps,
+} from '../../../lib/containers/QueryWrapperMenu'
 import { SynapseConstants } from '../../../lib/utils'
 import { GenericCardSchema } from '../../../lib/containers/GenericCard'
 
 type DemoState = {
-  token: string
   ownerId: string
   isLoading: boolean
   showMarkdown: boolean
@@ -12,53 +14,73 @@ type DemoState = {
   tabOne: QueryWrapperMenuProps
   tabTwo: QueryWrapperMenuProps
   tabThree: QueryWrapperMenuProps
+  tabFour: QueryWrapperMenuProps
   activeTab: number
 }
 /**
  * Demo of features that can be used from src/demo/utils/SynapseClient
  * module
  */
-class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState> {
+class QueryWrapperMenuDemo extends React.Component<
+  { rgbIndex: number },
+  DemoState
+> {
   /**
    * Maintain internal state of user session
    */
   constructor(props: any) {
     super(props)
-    const experimentalSql = "SELECT * FROM syn20337467 WHERE toolType = 'experimental' LIMIT 100" 
-    const computationalSql = "SELECT * FROM syn20337467 WHERE toolType = 'computational' LIMIT 100" 
+    const experimentalSql =
+      "SELECT * FROM syn20337467 WHERE toolType = 'experimental' LIMIT 100"
+    const computationalSql =
+      "SELECT * FROM syn20337467 WHERE toolType = 'computational' LIMIT 100"
     const genericCardSchema: GenericCardSchema = {
       title: 'Title',
       type: 'Generic Tool',
       subTitle: 'softwareType',
       description: 'summary',
       icon: 'icon',
-      secondaryLabels: [
-        'contributor',
-        'diagnosis',
-        'program',
-      ]
+      secondaryLabels: ['contributor', 'diagnosis', 'program'],
     }
     this.state = {
       isLoading: true,
       ownerId: '',
       showMarkdown: true,
-      activeTab: 2,
+      activeTab: 3,
+      tabFour: {
+        stackedBarChartConfiguration: {
+          loadingScreen: <div> Im loading as fast I can! </div>,
+        },
+        unitDescription: 'apples',
+        tableConfiguration: {
+          title: 'title',
+          synapseId: 'syn21085100',
+          markdownColumns: ['fruit', 'vegetable'],
+        },
+        menuConfig: [
+          {
+            facet: 'fruit',
+            sql: 'select * from syn21085100',
+          },
+        ],
+        rgbIndex: 3,
+      },
       tabThree: {
         stackedBarChartConfiguration: {
-          loadingScreen: <div> Im loading as fast I can! </div>
+          loadingScreen: <div> Im loading as fast I can! </div>,
         },
         unitDescription: 'datum',
         tableConfiguration: {
           title: 'title',
-          synapseId: 'syn11346063'
+          synapseId: 'syn11346063',
         },
         searchConfiguration: {
           searchable: [
             {
               columnName: 'study',
-              hintText: 'study name'
+              hintText: 'study name',
             },
-          ]
+          ],
         },
         menuConfig: [
           {
@@ -72,22 +94,22 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
           {
             sql: 'SELECT * FROM syn11346063 limit 1000',
           },
-        ] as MenuConfig[]
-        ,
-        rgbIndex: 5
+        ] as MenuConfig[],
+        rgbIndex: 5,
       },
-      tabTwo:
-      {
+      tabTwo: {
         name: 'Demo',
         unitDescription: 'persons',
         cardConfiguration: {
           genericCardSchema,
           type: SynapseConstants.MEDIUM_USER_CARD,
-          loadingScreen: <div style={{height: 450}}> I'm loading as fast I can! </div>
+          loadingScreen: (
+            <div style={{ height: 450 }}> I'm loading as fast I can! </div>
+          ),
         },
         rgbIndex: 1,
         facetAliases: {
-          grant: 'Grant'
+          grant: 'Grant',
         },
         accordionConfig: [
           {
@@ -95,8 +117,10 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
             cardConfiguration: {
               genericCardSchema,
               type: SynapseConstants.GENERIC_CARD,
-              loadingScreen: <div style={{height: 450}}> I'm loading as fast I can! </div>
-            },  
+              loadingScreen: (
+                <div style={{ height: 450 }}> I'm loading as fast I can! </div>
+              ),
+            },
             menuConfig: [
               {
                 facet: 'grant',
@@ -117,8 +141,10 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
             cardConfiguration: {
               genericCardSchema,
               type: SynapseConstants.GENERIC_CARD,
-              loadingScreen: <div style={{height: 450}}> I'm loading as fast I can! </div>
-            },  
+              loadingScreen: (
+                <div style={{ height: 450 }}> I'm loading as fast I can! </div>
+              ),
+            },
             menuConfig: [
               {
                 facet: 'program',
@@ -135,36 +161,38 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
             ],
           },
         ],
-      }
-    ,
+      },
       tabOne: {
         unitDescription: 'datum',
         tableConfiguration: {
           title: 'title',
-          synapseId: 'syn11346063'
+          synapseId: 'syn11346063',
         },
         searchConfiguration: {
           searchable: [
             {
               columnName: 'study',
-              hintText: 'study name'
+              hintText: 'study name',
             },
-          ]
+          ],
         },
         menuConfig: [
           {
             facet: 'diagnosis',
-            sql: 'SELECT study, assay, count(distinct id) AS files, concat(organ) AS organs' +
-            ' FROM syn17024112 WHERE species=\'Human\' AND assay=\'rnaSeq\' group by 1,2 order by 3 desc limit 1000',
+            sql:
+              'SELECT study, assay, count(distinct id) AS files, concat(organ) AS organs' +
+              " FROM syn17024112 WHERE species='Human' AND assay='rnaSeq' group by 1,2 order by 3 desc limit 1000",
           },
           {
             facet: 'PORTALS-712',
-            sql: 'SELECT dataType, assay, fileFormat, COUNT(distinct id) AS Files ' +
-              'FROM syn11346063 WHERE (study LIKE \'%MODEL-AD_5XFAD%\') GROUP BY 1, 2, 3 ORDER BY 4 DESC',
+            sql:
+              'SELECT dataType, assay, fileFormat, COUNT(distinct id) AS Files ' +
+              "FROM syn11346063 WHERE (study LIKE '%MODEL-AD_5XFAD%') GROUP BY 1, 2, 3 ORDER BY 4 DESC",
           },
           {
             facet: 'PORTALS-803',
-            sql: 'SELECT diagnosis AS "Diagnosis", sex as "Sex", dataType as "Data Type", assay as "Assay", count(distinct(id)) as "Files", count(distinct(specimenID)) as "Specimens", count(distinct(individualID)) as "Individuals" FROM syn11346063 GROUP BY 1,2,3,4 ORDER BY 1 DESC',
+            sql:
+              'SELECT diagnosis AS "Diagnosis", sex as "Sex", dataType as "Data Type", assay as "Assay", count(distinct(id)) as "Files", count(distinct(specimenID)) as "Specimens", count(distinct(individualID)) as "Individuals" FROM syn11346063 GROUP BY 1,2,3,4 ORDER BY 1 DESC',
           },
           {
             facet: 'study',
@@ -177,13 +205,10 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
           //   title: 'title',
           //   unitDescription: 'descriptive unit'
           // },
-        ] as MenuConfig[]
-        ,
-        rgbIndex: 5
-      }
-      ,
-      token: '',
-      version: 0
+        ] as MenuConfig[],
+        rgbIndex: 5,
+      },
+      version: 0,
     }
     this.handleChange = this.handleChange.bind(this)
     this.removeHandler = this.removeHandler.bind(this)
@@ -195,7 +220,7 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
     return [
       Math.floor((1 - alpha) * background[0] + alpha * color[0] + 0.5),
       Math.floor((1 - alpha) * background[1] + alpha * color[1] + 0.5),
-      Math.floor((1 - alpha) * background[2] + alpha * color[2] + 0.5)
+      Math.floor((1 - alpha) * background[2] + alpha * color[2] + 0.5),
     ]
   }
 
@@ -212,44 +237,58 @@ class QueryWrapperMenuDemo extends React.Component<{rgbIndex: number}, DemoState
   }
 
   public render(): JSX.Element {
-    const { 
-      activeTab
-    } = this.state
+    const { activeTab } = this.state
     let props: QueryWrapperMenuProps = {} as QueryWrapperMenuProps
     if (activeTab === 0) {
       props = this.state.tabOne
     } else if (activeTab === 1) {
-      props = this.state.tabTwo 
-    } else {
+      props = this.state.tabTwo
+    } else if (activeTab === 2) {
       props = this.state.tabThree
+    } else {
+      props = this.state.tabFour
     }
     return (
       <div className="container">
         <button
           className="btn btn-default"
-          onClick={() => {this.setState({activeTab: 0})}}
+          onClick={() => {
+            this.setState({ activeTab: 0 })
+          }}
         >
           With Group By
         </button>
         <button
           className="btn btn-default"
-          onClick={() => {this.setState({activeTab: 1})}}
+          onClick={() => {
+            this.setState({ activeTab: 1 })
+          }}
         >
           With Tools
         </button>
         <button
           className="btn btn-default"
-          onClick={() => {this.setState({activeTab: 2})}}
+          onClick={() => {
+            this.setState({ activeTab: 2 })
+          }}
         >
           Standard
         </button>
+        <button
+          className="btn btn-default"
+          onClick={() => {
+            this.setState({ activeTab: 3 })
+          }}
+        >
+          Table with markdown
+        </button>
         <h2>Demo of table</h2>
-          <QueryWrapperMenu
-            token={''}
-            isConsistent={true}
-            {...props}
-          />
-        </div>
+        <QueryWrapperMenu
+          token={'b05973c4-32ae-4cba-a668-559fdf6fdde5'}
+          isConsistent={true}
+          {...props}
+        />
+      </div>
     )
   }
 }
