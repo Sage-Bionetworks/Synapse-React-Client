bbe2c670031a513763af94965981a856e1fc4ad4
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-11-15 16:14:25 -0800

    update HasAccess based on designs

--- src/lib/containers/HasAccess.tsx
+++ src/lib/containers/HasAccess.tsx
@@ -64,7 +64,6 @@ export default class HasAccess extends React.Component<
   render() {
     const { restrictionInformation } = this.state
     const { synapseId } = this.props
-    // TODO: include a yellow alert icon with tooltip along with link
     const icon = restrictionInformation && restrictionInformation.hasUnmetAccessRequirement ? 
         <span className='fa-layers fa-fw' style={{marginRight:5}}>
           <FontAwesomeIcon icon={faCircle} className='SRC-warning-color' size='lg'/>
