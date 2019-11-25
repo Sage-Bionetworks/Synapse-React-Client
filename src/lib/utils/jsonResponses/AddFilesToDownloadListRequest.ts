4fb6f0987841725d9749c6860ba6bcd2d48125d3
Alina Gendel <alina.gendel@sagebase.org>
2019-11-25 10:09:55 -0800

    feat(downloadList): add 'addFilesToDownloadListRequest()'

---
+++ src/lib/utils/jsonResponses/AddFilesToDownloadListRequest.ts
@@ -0,0 +1,9 @@
+import { Query } from './Table/Query'
+
+//https://docs.synapse.org/rest/org/sagebionetworks/repo/model/file/AddFilesToDownloadListRequest.html
+export type AddFilesToDownloadListRequest = {
+  concreteType: string
+  //from the docs: The ID of the folder containing the files to add to the user's download list. Note: Only the files in this folder will be added to the download list, files in sub-folders will not be added. Should be null if setting a 'query'.
+  folderId?: string | null
+  query: Query
+}
