69e4c94bbd0433f2ce2a71b66d83af48a1312520
Michael Lee <michael.leem42@gmail.com>
2020-03-18 15:34:34 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Synapse-React-Client/precache-manifest.1d45a673b6160431a2877708ccb3a9bb.js"
+  "/Synapse-React-Client/precache-manifest.0f67c98195c30d88c5ce284f734fe5ba.js"
 );
 
 self.addEventListener('message', (event) => {
