34500a0c0e335338ff857de7c98aea4a7fb5366b
YongJoon Oh <yoh@xschildws-MacBook-Pro.local>
2020-03-24 14:49:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Synapse-React-Client/precache-manifest.54c2ba9ebd69c7ffc19fb597858955ba.js"
+  "/Synapse-React-Client/precache-manifest.bc6385ec37fd2c0a8d728db4d80bc3a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Synapse-React-Client/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
