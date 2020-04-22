0667356fc4ebfceee4d13e3c5796ea07463bedab
Michael Lee <michael.leem42@gmail.com>
2020-04-21 17:37:37 -0700

    Updates

---
+++ precache-manifest.45d3fa5cf35e539bb4806375d9f532e7.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23431d0ac2eed8255888b55770e0f5f",
+    "url": "/Synapse-React-Client/index.html"
+  },
+  {
+    "revision": "02f0d2e1518d04133e3f",
+    "url": "/Synapse-React-Client/static/css/2.11d466f2.chunk.css"
+  },
+  {
+    "revision": "38fb20136dc4c5f603dc",
+    "url": "/Synapse-React-Client/static/css/main.5f29cb42.chunk.css"
+  },
+  {
+    "revision": "02f0d2e1518d04133e3f",
+    "url": "/Synapse-React-Client/static/js/2.480e44fd.chunk.js"
+  },
+  {
+    "revision": "ba6d55c5718af57066b3bad083d34307",
+    "url": "/Synapse-React-Client/static/js/2.480e44fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fb20136dc4c5f603dc",
+    "url": "/Synapse-React-Client/static/js/main.6ac532c2.chunk.js"
+  },
+  {
+    "revision": "c21eef8c7a09dbebbdb2",
+    "url": "/Synapse-React-Client/static/js/runtime-main.5b75d8db.js"
+  },
+  {
+    "revision": "d8f80972135030bbfd82241a16aa46fd",
+    "url": "/Synapse-React-Client/static/media/DNA_Two.d8f80972.svg"
+  },
+  {
+    "revision": "1104e430b37f10f989089a81f19c9de7",
+    "url": "/Synapse-React-Client/static/media/Data2.1104e430.svg"
+  },
+  {
+    "revision": "c6301611c7b7608ee8c05c4167b8509a",
+    "url": "/Synapse-React-Client/static/media/brain.c6301611.svg"
+  },
+  {
+    "revision": "ed6d433ac9b20d9d3b317bd6395e8c3f",
+    "url": "/Synapse-React-Client/static/media/circle.ed6d433a.svg"
+  },
+  {
+    "revision": "94835d6057eba922f3de19e0daf9c0eb",
+    "url": "/Synapse-React-Client/static/media/columns.94835d60.svg"
+  },
+  {
+    "revision": "2b935894d99bdb5990210f9adf82b6c9",
+    "url": "/Synapse-React-Client/static/media/expand.2b935894.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/Synapse-React-Client/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/Synapse-React-Client/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/Synapse-React-Client/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/Synapse-React-Client/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/Synapse-React-Client/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/Synapse-React-Client/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/Synapse-React-Client/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/Synapse-React-Client/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/Synapse-React-Client/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/Synapse-React-Client/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/Synapse-React-Client/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/Synapse-React-Client/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/Synapse-React-Client/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/Synapse-React-Client/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/Synapse-React-Client/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "2dcc2eacd52c0dd775bab52aa2affe2c",
+    "url": "/Synapse-React-Client/static/media/file-dotted.2dcc2eac.svg"
+  },
+  {
+    "revision": "e64940c05806d6b24ea6c0c6b59a9725",
+    "url": "/Synapse-React-Client/static/media/json-form-tool-no-submissions.e64940c0.svg"
+  },
+  {
+    "revision": "64d772c3e73d74ab9fca30aedeec9f99",
+    "url": "/Synapse-React-Client/static/media/logo-white.64d772c3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Synapse-React-Client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5907af203b364d314935dbc7d00c12e",
+    "url": "/Synapse-React-Client/static/media/logo.c5907af2.svg"
+  },
+  {
+    "revision": "7e294e960c2fcacbdd85253b12fb64c4",
+    "url": "/Synapse-React-Client/static/media/mouse.7e294e96.svg"
+  },
+  {
+    "revision": "7500217a7747733cdde4c98daef2c6a0",
+    "url": "/Synapse-React-Client/static/media/resiliencead.7500217a.svg"
+  },
+  {
+    "revision": "199a80609e06354b060ef32c7e7659d1",
+    "url": "/Synapse-React-Client/static/media/shrink.199a8060.svg"
+  },
+  {
+    "revision": "bc52e72be82449e98220e964df044c96",
+    "url": "/Synapse-React-Client/static/media/study-active.bc52e72b.svg"
+  },
+  {
+    "revision": "b012f80b396b048881abcbfa63f2abd1",
+    "url": "/Synapse-React-Client/static/media/study-complete.b012f80b.svg"
+  }
+]);
