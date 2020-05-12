270aeec3c5fa64af6ba916d19089378af062b8af
Michael Lee <michael.leem42@gmail.com>
2020-05-12 16:10:40 -0700

    remove svg component import and make svg assets functional components

---
+++ src/lib/assets/icons/columns.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+export default (
+  <svg
+    className="SRC-column-light-theme"
+    style={{ display: 'block' }}
+    width="27"
+    height="28"
+    viewBox="0 0 27 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="4" y="6" width="5.66667" height="17" fill="white" />
+    <rect x="11.0837" y="6" width="5.66667" height="17" fill="white" />
+    <rect
+      className="SRC-light-column"
+      x="18.6663"
+      y="6.5"
+      width="4.66667"
+      height="16"
+      stroke="white"
+    />
+  </svg>
+)
