59b792bee979f6d3faf2f8e224749e4943de7563
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-05-19 23:49:01 -0700

    fix QueryResultBundle import

--- src/lib/containers/query_wrapper_plot_nav/TopLevelControls.tsx
+++ src/lib/containers/query_wrapper_plot_nav/TopLevelControls.tsx
@@ -12,7 +12,7 @@ import { ColumnSelection } from '../table/table-top/ColumnSelection'
 import { SynapseClient } from '../../utils'
 import { ElementWithTooltip } from '../widgets/ElementWithTooltip'
 import { cloneDeep } from 'lodash-es'
-import { QueryResultBundle } from 'lib/utils/synapseTypes'
+import { QueryResultBundle } from 'lib/utils/synapseTypes/Table/QueryResultBundle'
 
 library.add(faSearch)
 library.add(faFilter)
