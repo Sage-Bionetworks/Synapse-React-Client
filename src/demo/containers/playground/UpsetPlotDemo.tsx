7d30f2d74e3c6abcbc6c0ec55e167ae84bf0a3c9
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-08-11 15:13:48 -0700

    render test

--- src/demo/containers/playground/UpsetPlotDemo.tsx
+++ src/demo/containers/playground/UpsetPlotDemo.tsx
@@ -5,8 +5,8 @@ export const UpsetPlotDemo = () => {
   return (
     <UpsetPlot
       entityId='syn20821313'
-      sql='SELECT distinct individualID, assay FROM syn20821313 where individualID is not null order by individualID'
-      maxBarCount={20}
+      sql='SELECT distinct individualID, assay FROM syn20821313 where individualID is not null'
+      // maxBarCount={20}
       setName='Individuals (#) per Assay'
       combinationName='Individuals (#) '
     />
