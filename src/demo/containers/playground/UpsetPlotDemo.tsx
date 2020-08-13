2608652828fe2d34bbf5beb2f84df2cad4a335e2
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-08-13 11:07:02 -0700

    PORTALS-1522: improve error handling, and check for null (annotation value)

--- src/demo/containers/playground/UpsetPlotDemo.tsx
+++ src/demo/containers/playground/UpsetPlotDemo.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import UpsetPlot from 'lib/containers/UpsetPlot'
 
-export const UpsetPlotDemo = () => {
+type UpsetPlotDemoProps = {
+  token: string
+}
+
+export const UpsetPlotDemo: React.FunctionComponent<UpsetPlotDemoProps> = ({token}) => {
   return (
     <UpsetPlot
+      token={token}
       sql={"SELECT distinct individualID, assay FROM syn20821313 where individualID is not null"}
-      rgbIndex={3}
+      rgbIndex={0}
       maxBarCount={20}
       setName='Individuals (#) per Assay'
       combinationName='Individuals (#)'
--- src/lib/containers/UpsetPlot.tsx
+++ src/lib/containers/UpsetPlot.tsx
@@ -5,6 +5,7 @@ import { SynapseConstants, SynapseClient } from '../utils'
 import { SizeMe } from 'react-sizeme'
 import getColorPallette from './ColorGradient'
 import { parseEntityIdFromSqlStatement } from '../utils/functions/sqlFunctions'
+import { Alert } from 'react-bootstrap'
 
 export type UpsetPlotProps = {
   sql: string, // first column should contain values, second column should contain a single set value.  ie. SELECT distinct individualID, assay FROM syn20821313
@@ -37,6 +38,7 @@ const UpsetPlot: React.FunctionComponent<UpsetPlotProps> = ({
 }) => {
   const [isLoading, setIsLoading] = useState<boolean>()
   const [data, setData] = useState<UpsetPlotData>()
+  const [error, setError] = useState<string>()
   const [selection, setSelection] = useState(null as ISetLike<any> | null)
 
   const { colorPalette } = getColorPallette(rgbIndex, 2)
@@ -48,65 +50,72 @@ const UpsetPlot: React.FunctionComponent<UpsetPlotProps> = ({
     let isCancelled: boolean = false
     const getPlotData = async () => {
       setIsLoading(true)
-      const entityId = parseEntityIdFromSqlStatement(sql)
-      const partMask = SynapseConstants.BUNDLE_MASK_QUERY_RESULTS
-      const queryRequest: QueryBundleRequest = {
-        partMask,
-        concreteType: 'org.sagebionetworks.repo.model.table.QueryBundleRequest',
-        entityId,
-        query: {
-          sql
-        },
-      }
-      const {
-        queryResult
-      } = await SynapseClient.getFullQueryTableResults(
-        queryRequest,
-        token
-      )
-      // transform query data into plot data, and store.
-      // collect all values for each key
-      const keyValuesMap = {}
-      // keyValuesMap looks like { 'A': {name: 'A', sets: ['S1', 'S2'] }, 'B': { name: 'B', sets: ['S1'] }, ... }
-      // It's a little redudant, but makes the next step much easier.
-      
-      const caseInsensitiveSetNames = {}
-      // caseInsensitiveSetNames looks like { 'RNASEQ': 'rnaSeq', 'NOMe-SEQ': 'NOMe-seq'}.
+      try {
+        const entityId = parseEntityIdFromSqlStatement(sql)
+        const partMask = SynapseConstants.BUNDLE_MASK_QUERY_RESULTS
+        const queryRequest: QueryBundleRequest = {
+          partMask,
+          concreteType: 'org.sagebionetworks.repo.model.table.QueryBundleRequest',
+          entityId,
+          query: {
+            sql
+          },
+        }
+        const {
+          queryResult
+        } = await SynapseClient.getFullQueryTableResults(
+          queryRequest,
+          token
+        )
+        // transform query data into plot data, and store.
+        // collect all values for each key
+        const keyValuesMap = {}
+        // keyValuesMap looks like { 'A': {name: 'A', sets: ['S1', 'S2'] }, 'B': { name: 'B', sets: ['S1'] }, ... }
+        // It's a little redudant, but makes the next step much easier.
+        
+        const caseInsensitiveSetNames = {}
+        // caseInsensitiveSetNames looks like { 'RNASEQ': 'rnaSeq', 'NOMe-SEQ': 'NOMe-seq'}.
 
-      for (const row of queryResult.queryResults.rows) {
-        for (let j = 1; j < row.values.length; j += 1) {
-          const rowValues:string[] = row.values
-          const key = rowValues[0]
-          let newValue = rowValues[j]
-          keyValuesMap[key] = keyValuesMap[key] || {}
-          keyValuesMap[key].name = key
+        for (const row of queryResult.queryResults.rows) {
+          for (let j = 1; j < row.values.length; j += 1) {
+            const rowValues:string[] = row.values
+            const key = rowValues[0]
+            let newValue = rowValues[j]
+            keyValuesMap[key] = keyValuesMap[key] || {}
+            keyValuesMap[key].name = key
 
-          keyValuesMap[key].sets = keyValuesMap[key].sets || []
-          const found = keyValuesMap[key].sets.find((v: any) => v == newValue)
-          if (!found) {
-            if (caseInsensitiveSetNames[newValue.toUpperCase()]) {
-              newValue = caseInsensitiveSetNames[newValue.toUpperCase()]
-            } else {
-              caseInsensitiveSetNames[newValue.toUpperCase()] = newValue
+            keyValuesMap[key].sets = keyValuesMap[key].sets || []
+            const found = keyValuesMap[key].sets.find((v: any) => v == newValue)
+            if (!found && newValue) {
+              if (caseInsensitiveSetNames[newValue.toUpperCase()]) {
+                newValue = caseInsensitiveSetNames[newValue.toUpperCase()]
+              } else {
+                caseInsensitiveSetNames[newValue.toUpperCase()] = newValue
+              }
+              keyValuesMap[key].sets.push(newValue)
             }
-            keyValuesMap[key].sets.push(newValue)
           }
         }
-      }
-      // now create the expected elems set
-      const elems: any[] = Object.values(keyValuesMap)
-      // elems looks like [{ name: 'A', sets: ['S1', 'S2'] }, { name: 'B', sets: ['S1'] }, ...]
-      const sets = extractSets(elems);
-      const combinations = generateCombinations(sets, {
-        type: 'intersection',
-        min: 1,
-        limit: maxBarCount,
-        order: 'cardinality:desc',
-      })
-      if (!isCancelled) {
-        setData({sets, combinations})
-        setIsLoading(false)
-      }
+        // now create the expected elems set
+        const elems: any[] = Object.values(keyValuesMap)
+        // elems looks like [{ name: 'A', sets: ['S1', 'S2'] }, { name: 'B', sets: ['S1'] }, ...]
+        const sets = extractSets(elems);
+        const combinations = generateCombinations(sets, {
+          type: 'intersection',
+          min: 1,
+          limit: maxBarCount,
+          order: 'cardinality:desc',
+        })
+        if (!isCancelled) {
+          setData({sets, combinations})
+          setIsLoading(false)
+        }
+      } catch (err) {
+        if (!isCancelled) {
+          setIsLoading(false)
+          setError(err.toString())
+        }
+      }      
     }
     getPlotData()
     return () => {
@@ -141,6 +150,15 @@ const UpsetPlot: React.FunctionComponent<UpsetPlotProps> = ({
             </div>            
           )}
         </SizeMe>
+      }
+      {error && 
+        <Alert
+          transition={'div'}
+          variant={'danger'}
+          show={true}
+        >
+          {error}
+        </Alert>      
       }
     </>
   )
