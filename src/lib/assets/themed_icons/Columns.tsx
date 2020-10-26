c0e4da7a02d3dde85174fab5ef87a4847b2d2348
emmanmills <emmabuy@gmail.com>
2020-10-26 11:22:13 -0700

    Update column icon from design

--- src/lib/assets/themed_icons/Columns.tsx
+++ src/lib/assets/themed_icons/Columns.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 
 export const Columns = () => (
-  <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="5.33333" height="16" fill="#ffffff"/>
-    <rect x="6.66797" width="5.33333" height="16" fill="#ffffff"/>
-    <rect x="13.832" y="0.5" width="4.33333" height="15" fill="none" stroke="#ffffff"/>
+  <svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="5.66667" height="17" fill="white"/>
+    <rect x="7.08368" width="5.66667" height="17" fill="white"/>
+    <rect x="14.6663" y="0.5" width="4.66667" height="16" stroke="white" fill="none"/>
   </svg>
 )
--- src/lib/assets/themed_icons/ColumnsDark.tsx
+++ src/lib/assets/themed_icons/ColumnsDark.tsx
@@ -1,35 +1,9 @@
 import * as React from 'react'
 
 export const ColumnsDark = () => (
-  <svg
-    className="SRC-column-dark-theme"
-    width="27"
-    height="28"
-    viewBox="0 0 27 28"
-    fill="#white"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect
-      className="SRC-primary-fill-color"
-      x="4"
-      y="6"
-      width="5.66667"
-      height="17"
-    />
-    <rect
-      className="SRC-primary-fill-color"
-      x="11.0837"
-      y="6"
-      width="5.66667"
-      height="17"
-    />
-    <rect
-      style={{ fill: 'none' }}
-      className="SRC-primary-stroke-color"
-      x="18.6663"
-      y="6.5"
-      width="4.66667"
-      height="16"
-    />
+  <svg width="20" height="17" viewBox="0 0 20 17" xmlns="http://www.w3.org/2000/svg">
+    <rect className="SRC-primary-fill-color" width="5.66667" height="17" />
+    <rect className="SRC-primary-fill-color" x="7.08368" width="5.66667" height="17" />
+    <rect className="SRC-primary-stroke-color" x="14.6663" y="0.5" width="4.66667" height="16" stroke="#ffffff" fill="#ffffff" />
   </svg>
 )
