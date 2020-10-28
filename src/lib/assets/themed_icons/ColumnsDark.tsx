0f9b7e39727d92d86eec49b69fdb4389cc04acb7
emmanmills <emmabuy@gmail.com>
2020-10-28 11:53:23 -0700

    Code cleanup

--- src/lib/assets/themed_icons/ColumnsDark.tsx
+++ src/lib/assets/themed_icons/ColumnsDark.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 export const ColumnsDark = () => (
-  <svg data-icon="columns-dark"width="20" height="17" viewBox="0 0 20 17" xmlns="http://www.w3.org/2000/svg">
+  <svg data-icon="columns-dark" width="20" height="17" viewBox="0 0 20 17" xmlns="http://www.w3.org/2000/svg">
     <rect className="SRC-primary-fill-color" width="5.66667" height="17" />
     <rect className="SRC-primary-fill-color" x="7.08368" width="5.66667" height="17" />
     <rect className="SRC-primary-stroke-color" x="14.6663" y="0.5" width="4.66667" height="16" stroke="#ffffff" fill="#ffffff" />
