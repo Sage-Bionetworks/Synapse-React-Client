cb08dd7c49f29f5e7ba87e5a57e31bb8022c0ac7
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2020-11-03 14:24:33 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Synapse-React-Client/precache-manifest.765d72e9a8c66573ac51ed031fb45a82.js"
+  "/Synapse-React-Client/precache-manifest.0ae388ca9da06128694223eaef32e449.js"
 );
 
 self.addEventListener('message', (event) => {
