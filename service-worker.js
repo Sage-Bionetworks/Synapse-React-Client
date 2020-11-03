e696327172cf8a735cfcede1534c6c5eca98561c
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2020-11-03 15:36:52 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/branch/SWC-5723/Synapse-React-Client/precache-manifest.88b94c5207000c3058574ca7d296cb1a.js"
+  "/Synapse-React-Client/precache-manifest.765d72e9a8c66573ac51ed031fb45a82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/branch/SWC-5723/Synapse-React-Client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Synapse-React-Client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
