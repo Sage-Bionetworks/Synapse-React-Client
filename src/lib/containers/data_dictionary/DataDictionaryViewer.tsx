9601a1a9b58a84b55a0e1295c5f9cc2f8674c292
Batch Mosqueda <batch@genui.com>
2020-11-25 13:02:10 -0800

    added colors to nodes

--- src/lib/containers/data_dictionary/DataDictionaryViewer.tsx
+++ src/lib/containers/data_dictionary/DataDictionaryViewer.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, ReactElement, useCallback } from 'react'
+import React, {
+  useEffect,
+  useState,
+  ReactElement,
+  useCallback,
+  useRef,
+} from 'react'
 //@ts-ignore
 import Graph from 'react-graph-network'
 import { stateData } from './state/DataState'
@@ -17,6 +23,10 @@ import ViewTypeChooser from './ViewTypeChooser'
 import GraphNetworkNode from './GraphNetworkNode'
 import EntityDetailViewer from './EntityDetailViewer'
 import GraphNetworkLine from './GraphNetworkLine'
+import {
+  COLOR_PALETTE_EVEN,
+  COLOR_PALETTE_ODD,
+} from 'lib/utils/functions/colorPalette'
 
 export interface DataDictionaryViewerProps {
   title: string
@@ -30,6 +40,7 @@ function DataDictionaryViewer({
   const [graphNetworkData, setGraphNetworkData] = useState<GraphNetworkData>()
   const [clickedNode, setClickedNode] = useState<DataDictionaryData>()
   const [deps, setDeps] = useState<DepState>({} as DepState)
+  const nodeColorRefs = useRef<{ [key: string]: string }>({})
 
   const onNodeClick = useCallback(
     (id: string) => (event: React.MouseEvent<SVGCircleElement, MouseEvent>) => {
@@ -54,6 +65,30 @@ function DataDictionaryViewer({
     const nodes: Array<GraphNodeData> = []
     const links: Array<GraphNodeLinkData> = []
 
+    if (deps[viewType]) {
+      // need to convert object into array so we can sort them with the most children first.
+      const res = Object.keys(deps[viewType])
+        .map(k => {
+          return { key: k, value: deps[viewType][k] }
+        })
+        .sort((a, b) => {
+          return b.value.length - a.value.length
+        })
+
+      // assign color pair for parent and children using the ODD/EVEN scheme in the palette
+      res.forEach((item, idx) => {
+        if (!(item.key in nodeColorRefs.current)) {
+          //TODO: check if there is enough color for the number of nodes
+          nodeColorRefs.current[item.key] = COLOR_PALETTE_EVEN[idx]
+          item.value.forEach(v => {
+            if (!(v in nodeColorRefs.current)) {
+              nodeColorRefs.current[v] = COLOR_PALETTE_ODD[idx]
+            }
+          })
+        }
+      })
+    }
+
     for (const id in deps[viewType]) {
       const node = nodes.find(entity => entity.id === id)
       const currentEntity:
@@ -61,7 +96,12 @@ function DataDictionaryViewer({
         | DataDictionaryData
         | undefined = !node ? data.find(entity => entity.id === id) : node
       if (currentEntity) {
-        nodes.push({ ...currentEntity, onNodeClick, viewType })
+        nodes.push({
+          ...currentEntity,
+          onNodeClick,
+          viewType,
+          nodeColor: nodeColorRefs.current[id],
+        })
         deps[viewType][id].forEach((childId: string) => {
           links.push({
             source: id,
@@ -72,7 +112,7 @@ function DataDictionaryViewer({
       }
     }
     setGraphNetworkData({ nodes, links })
-  }, [data, deps, viewType, onNodeClick])
+  }, [data, deps, viewType, onNodeClick, nodeColorRefs])
 
   if (!graphNetworkData) {
     return <></>
@@ -91,12 +131,12 @@ function DataDictionaryViewer({
           NodeComponent={GraphNetworkNode}
           LineComponent={GraphNetworkLine}
           id={`dd-graph`}
-          hoverOpacity={0.7}
+          hoverOpacity={0.3}
           enableDrag={true}
           zoomDepth={3}
           enableZoomOut={true}
-          nodeDistance={200}
-          pullIn={true}
+          nodeDistance={50}
+          pullIn={false}
         />
       </div>
       <EntityDetailViewer
