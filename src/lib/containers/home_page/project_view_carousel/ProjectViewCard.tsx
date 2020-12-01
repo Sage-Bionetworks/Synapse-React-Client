8b4966bfa06bc2151a3dda77798f7946bb8b292a
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2020-12-01 16:02:05 -0500

    add a card that displays info from a project view

---
+++ src/lib/containers/home_page/project_view_carousel/ProjectViewCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+export type ProjectCardProps = {
+  projectName: string
+  projectDescription: string
+  synId: string
+  image?: JSX.Element
+}
+
+export const ProjectViewCard: React.FunctionComponent<
+  ProjectCardProps & React.HTMLAttributes<HTMLDivElement>
+> = ({ projectName, projectDescription, synId, image, ...domProps }) => {
+  return (
+    <div
+      {...domProps}
+      className={`cardContainer ProjectViewCard bootstrap-4-backport ${
+        domProps.className ?? ''
+      }`}
+    >
+      <div className={'ProjectViewCard__Image'}>{image}</div>
+      <div>
+        <div className="ProjectViewCard__ProjectName">{projectName}</div>
+        <div className="ProjectViewCard__ProjectDescription">
+          {projectDescription}
+        </div>
+      </div>
+      <Button
+        variant="primary"
+        className="ProjectViewCard__ViewProjectButton"
+        onClick={() =>
+          window.open(`https://www.synapse.org/#!Synapse:${synId}`, '_blank')
+        }
+      >
+        View Project
+      </Button>
+    </div>
+  )
+}
