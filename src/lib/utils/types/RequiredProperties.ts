be697aac0808eb93885d96ba39df680d7356d4d5
Ziming Dong <ziming.dong@sagebase.org>
2020-12-02 03:46:30 -0800

    add documentation for RequiredProperties

--- src/lib/utils/types/RequiredProperties.ts
+++ src/lib/utils/types/RequiredProperties.ts
@@ -1,2 +1,23 @@
+/**
+ * Makes fields for a type required.
+ * For example:
+ *
+ * type FooBarBaz = {
+ *   foo: string
+ *   bar?: string
+ *   baz?: string
+ * }
+ *
+ * type RequireBar = RequiredProperties<FooBarBaz, 'baz'>
+ * Would result in :
+ * RequireBar === {
+ *   foo: string
+ *   bar?: string
+ *   baz: string // this is now required
+ * }
+ *
+ * To require multiple properties use String Literal union:
+ * type RequireBarBaz = RequiredProperties<FooBarBaz, 'bar' | 'baz'>
+ */
 export type RequiredProperties<T, K extends keyof T> = Omit<T, K> &
   Required<Pick<T, K>>
