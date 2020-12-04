aa3275a585814042959b2245cd915627ebf2c91d
Batch Mosqueda <batch@genui.com>
2020-12-04 12:47:52 -0800

    updated graphloader test

--- src/__tests__/lib/containers/data_schema/GraphLoader.test.tsx
+++ src/__tests__/lib/containers/data_schema/GraphLoader.test.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import GraphLoader from 'lib/containers/data_schema/GraphLoader'
+import { VIEW_TYPES } from 'lib/containers/data_schema/constants'
+
+const onNodeClick = jest.fn()
+const nodes = [
+  {
+    id: 'schema:Text',
+    type: ['schema:DataType', 'rdfs:Class'],
+    description: 'Data type: Text.',
+    label: 'Text',
+    parentIds: [],
+    attribute: ``,
+    nodeColor: `red`,
+    numOfDependents: 3,
+    onNodeClick,
+    required: false,
+    requiredDependencies: [],
+    validationRules: [],
+    validValues: [],
+    domainIncludes: [],
+    requiresComponent: [],
+    source: ``,
+    viewType: VIEW_TYPES.REQUIRES_COMPONENT,
+  },
+  {
+    id: 'schema:Text',
+    type: ['schema:DataType', 'rdfs:Class'],
+    description: 'Data type: Text.',
+    label: 'Text',
+    parentIds: [],
+    attribute: ``,
+    nodeColor: `red`,
+    numOfDependents: 3,
+    onNodeClick,
+    required: false,
+    requiredDependencies: [],
+    validationRules: [],
+    validValues: [],
+    domainIncludes: [],
+    requiresComponent: [],
+    source: ``,
+    viewType: VIEW_TYPES.REQUIRES_COMPONENT,
+  },
+]
 
 test('GraphLoader loads and displays text', async () => {
-  render(<GraphLoader />)
+  render(<GraphLoader nodes={nodes} />)
 
   expect(screen.getByAltText('Sage Bionetworks logo')).toBeDefined()
-  expect(screen.getByText(`Generating chart, please wait...`)).toBeDefined()
+  expect(screen.getByText(`Generating 1 nodes, please wait...`)).toBeDefined()
 })
