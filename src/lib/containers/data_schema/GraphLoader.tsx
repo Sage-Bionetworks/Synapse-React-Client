88c804a5c02831413ad3eb57e7442682a538d553
Batch Mosqueda <batch@genui.com>
2020-12-04 11:30:57 -0800

    updated graphloader with progress bar

--- src/lib/containers/data_schema/GraphLoader.tsx
+++ src/lib/containers/data_schema/GraphLoader.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { GraphNodeData } from './types/IDataSchemaTypes'
+import { BarLoader } from 'react-spinners'
 
-export default function GraphLoader() {
+interface GraphLoaderProps {
+  nodes: GraphNodeData[]
+}
+
+export default function GraphLoader({ nodes: nodes }: GraphLoaderProps) {
+  const numNodes = nodes.length - 1 // deduct hiddenRoot node from count.
+  const loadingData = numNodes <= 0 || !numNodes ? 'chart' : `${numNodes} nodes`
+
   return (
     <div className={`graphLoader`}>
       <img
         alt={'Sage Bionetworks logo'}
         src="https://s3.amazonaws.com/static.synapse.org/sage-bionetworks-logo.svg"
       />
-      <p className={`graphLoaderText`}>{`Generating chart, please wait...`}</p>
+      <div className={`graphTextAndBar`}>
+        <p
+          className={`graphLoaderText`}
+        >{`Generating ${loadingData}, please wait...`}</p>
+        <div className={`barLoader`}>
+          <BarLoader color="#878787" loading={true} height={5} />
+        </div>
+      </div>
     </div>
   )
 }
