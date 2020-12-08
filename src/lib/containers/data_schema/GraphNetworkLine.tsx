e9865e9ce0a14413d917ddfcb65c1d0d7eb4682c
Batch Mosqueda <batch@genui.com>
2020-12-08 15:00:36 -0800

    patch/refactor: [#175881548] added node collision prop and node radius, added arrowhead to lines.

--- src/lib/containers/data_schema/GraphNetworkLine.tsx
+++ src/lib/containers/data_schema/GraphNetworkLine.tsx
@@ -9,11 +9,30 @@ export default function GraphNetworkLine({
   ...props
 }: GraphNetworkLineProps) {
   return (
-    <line
-      {...props}
-      stroke={linkColor}
-      strokeWidth={3}
-      strokeOpacity={source === 'hiddenRoot' ? 0 : 1}
-    />
+    <g>
+      <defs>
+        <marker
+          id={`${source}_arrowhead`}
+          markerWidth="5"
+          markerHeight="3.5"
+          refX="16.25"
+          refY="1.75"
+          orient="auto"
+          stroke={linkColor}
+          fill={linkColor}
+        >
+          <polygon points="-1.5 0, 5 1.75, -1.5 3.5" />
+        </marker>
+      </defs>
+      <line
+        {...props}
+        stroke={linkColor}
+        strokeWidth={3}
+        strokeOpacity={source === 'hiddenRoot' ? 0 : 1}
+        markerEnd={
+          source != 'hiddenRoot' ? `url(#${source}_arrowhead)` : undefined
+        }
+      />
+    </g>
   )
 }
