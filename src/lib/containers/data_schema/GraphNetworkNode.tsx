d8758ce1b30e03c8fb3b7ae949408e93f4cac4d8
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-12-11 12:53:54 -0800

    patch: [#176020903] Change node shape based on type.

--- src/lib/containers/data_schema/GraphNetworkNode.tsx
+++ src/lib/containers/data_schema/GraphNetworkNode.tsx
@@ -1,5 +1,5 @@
 import { unCamelCase } from 'lib/utils/functions/unCamelCase'
-import React, { useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { GraphNodeData } from './types/IDataSchemaTypes'
 
 interface GraphNetworkNodeProps {
@@ -8,41 +8,97 @@ interface GraphNetworkNodeProps {
 }
 
 export default function GraphNetworkNode({
-  node: { id, label, type, onNodeClick, nodeColor },
+  node: { attribute, id, label, type, onNodeClick, nodeColor },
   nodeRadius,
-}: GraphNetworkNodeProps) {
+}: GraphNetworkNodeProps): ReactElement {
   const [hoverClass, setHoverClass] = useState('mouseOffNode')
-  const radius = nodeRadius
-  const isPropertyType = type.includes('rdf:Property')
-  const nodeLabel = unCamelCase(label)
+  const radius = id !== 'hiddenRoot' ? nodeRadius : 0
+  const nodeDisplay = unCamelCase(attribute || label)
 
   return (
     <g
       className={`entity ${hoverClass}`}
-      onClick={onNodeClick(id)}
-      onMouseEnter={() => setHoverClass('mouseOnNode')}
-      onMouseLeave={() => setHoverClass('mouseOffNode')}
+      onClick={id !== 'hiddenRoot' ? onNodeClick(id) : undefined}
+      onMouseEnter={
+        id !== 'hiddenRoot' ? () => setHoverClass('mouseOnNode') : undefined
+      }
+      onMouseLeave={
+        id !== 'hiddenRoot' ? () => setHoverClass('mouseOffNode') : undefined
+      }
       pointerEvents={id === 'hiddenRoot' ? 'none' : undefined}
       opacity={id === 'hiddenRoot' ? 0 : 1}
     >
-      <circle
-        fill={isPropertyType ? 'white' : nodeColor}
-        stroke={isPropertyType ? nodeColor : undefined}
-        strokeWidth={isPropertyType ? 2 : undefined}
-        r={isPropertyType ? radius - 2 : radius}
-        className={`shape`}
-      />
-      <foreignObject
-        x={-Math.abs(radius - 5)}
-        y={-Math.abs(radius - 5)}
-        width={radius * 2 - 10}
-        height={radius * 2 - 10}
-        className={`shape`}
-      >
-        <div title={nodeLabel} className={`text-entity`}>
-          <span style={{ borderColor: `${nodeColor}` }}>{nodeLabel}</span>
-        </div>
-      </foreignObject>
+      {type.includes('schema:DataType') ? (
+        <Square nodeColor={nodeColor} radius={radius} />
+      ) : type.includes('rdf:Property') ? (
+        <Hexagon nodeColor={nodeColor} radius={radius} />
+      ) : (
+        <Circle nodeColor={nodeColor} radius={radius} />
+      )}
+
+      {id !== 'hiddenRoot' && (
+        <foreignObject
+          x={-Math.abs(radius - 5)}
+          y={-Math.abs(radius - 5)}
+          width={radius * 2 - 10}
+          height={radius * 2 - 10}
+          className={`shape${
+            type.includes('schema:DataType')
+              ? ` square`
+              : type.includes('rdf:Property')
+              ? ` hexagon`
+              : ``
+          }`}
+        >
+          <div title={nodeDisplay} className={`text-entity`}>
+            <span style={{ borderColor: `${nodeColor}` }}>{nodeDisplay}</span>
+          </div>
+        </foreignObject>
+      )}
     </g>
+  )
+}
+
+interface ShapeProps {
+  nodeColor: string
+  radius: number
+}
+
+function Circle({ nodeColor, radius }: ShapeProps): ReactElement {
+  return <circle fill={nodeColor} r={radius} className={`shape`} />
+}
+
+function Hexagon({ nodeColor, radius }: ShapeProps): ReactElement {
+  const points = [0, 1, 2, 3, 4, 5, 6]
+    .map((n, i) => {
+      var angle_deg = 60 * i - 30
+      var angle_rad = (Math.PI / 180) * angle_deg
+      return [radius * Math.cos(angle_rad), radius * Math.sin(angle_rad)]
+    })
+    .map(p => p.join(','))
+    .join(' ')
+
+  return (
+    <polygon
+      fill={`white`}
+      points={points}
+      stroke={nodeColor}
+      strokeWidth={2}
+      className={`shape`}
+    />
+  )
+}
+
+function Square({ nodeColor, radius }: ShapeProps): ReactElement {
+  const size = radius * 2
+  return (
+    <rect
+      fill={nodeColor}
+      className={`shape`}
+      width={size}
+      height={size}
+      x={-Math.abs(radius)}
+      y={-Math.abs(radius)}
+    />
   )
 }
