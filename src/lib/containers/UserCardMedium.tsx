30174c64cc772de55ee4442c3805a9f9da1fd815
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2020-12-14 15:55:08 -0500

    rm unintended change

--- src/lib/containers/UserCardMedium.tsx
+++ src/lib/containers/UserCardMedium.tsx
@@ -277,7 +277,7 @@ export default class UserCardMedium extends React.Component<
       return (
         <div
           style={{ border: '1px solid #DDDDDF', backgroundColor: 'white' }}
-          className={`cardContainer SRC-userCard SRC-userCardMediumUp ${
+          className={`SRC-userCard SRC-userCardMediumUp ${
             isContextMenuOpen ? 'SRC-hand-cursor' : ''
           }`}
           onClick={isContextMenuOpen ? this.toggleContextMenu : undefined}
