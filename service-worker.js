9ae5c71ecc8506a6a2e158164f0cf1e96c217e24
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-01-11 15:36:47 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4f4be7aaa928056dbf2b94b45ba658b1.js"
+  "precache-manifest.0a1624d2b8346e2ecdd69579b3031e25.js"
 );
 
 self.addEventListener('message', (event) => {
