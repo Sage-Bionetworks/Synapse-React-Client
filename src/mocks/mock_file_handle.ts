38a8fc1624c843e84d4eabd30f783121affb8b1a
emmanmills <emmabuy@gmail.com>
2021-01-13 11:27:29 -0800

    add line break

--- src/mocks/mock_file_handle.ts
+++ src/mocks/mock_file_handle.ts
@@ -1,7 +1,8 @@
 import { FileHandle } from '../lib/utils/synapseTypes/'
 
 export const mockFileHandle:FileHandle = {
-  id: "8781190", etag: "abc-deg-4028-8eb8-aaaaa",
+  id: "8781190",
+  etag: "abc-deg-4028-8eb8-aaaaa",
   createdBy: "33203344343",
   createdOn: "2016-05-09T22:08:22.000Z",
   concreteType: "org.sagebionetworks.repo.model.file.S3FileHandle",
