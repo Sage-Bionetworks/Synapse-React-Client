12f7448360201cdfe585020ac419af5f6fa58a0d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-02-10 13:53:43 -0500

    Enums should be strings

--- src/lib/utils/synapseTypes/EntityChildren.ts
+++ src/lib/utils/synapseTypes/EntityChildren.ts
@@ -3,21 +3,21 @@ import { EntityType } from './EntityType'
 
 // https://docs.synapse.org/rest/org/sagebionetworks/repo/model/entity/SortBy.html
 export enum SortBy {
-  NAME,
-  CREATED_ON,
-  MODIFIED_ON,
+  NAME = 'NAME',
+  CREATED_ON = 'CREATED_ON',
+  MODIFIED_ON = 'MODIFIED_ON',
 }
 
 // https://docs.synapse.org/rest/org/sagebionetworks/repo/model/entity/Direction.html
 export enum Direction {
-  ASC,
-  DESC,
+  ASC = 'ASC',
+  DESC = 'DESC',
 }
 
 // https://docs.synapse.org/rest/org/sagebionetworks/repo/model/EntityChildrenRequest.html
 export type EntityChildrenRequest = {
   parentId: string
-  nextPageToken?: string
+  nextPageToken?: string | null
   includeTypes: Array<EntityType>
   sortBy?: SortBy
   sortDirection?: Direction
