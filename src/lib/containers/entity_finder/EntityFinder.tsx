4b17a117a54eaacd6914323830822f2fb4e6bb51
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-03-23 14:22:08 -0400

    removed prop

--- src/lib/containers/entity_finder/EntityFinder.tsx
+++ src/lib/containers/entity_finder/EntityFinder.tsx
@@ -333,7 +333,6 @@ export const EntityFinder: React.FunctionComponent<EntityFinderProps> = ({
                         <TreeView
                           sessionToken={sessionToken}
                           setDetailsViewConfiguration={setConfigFromTreeView}
-                          showFakeRootNode={true}
                           showDropdown={true}
                           visibleTypes={visibleTypesInTree}
                           initialScope={initialScope}
