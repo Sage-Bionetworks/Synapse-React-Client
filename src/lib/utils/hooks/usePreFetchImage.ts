1cf7e7602404343fdea68d0b6c2d89f1ff145f70
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-04-13 10:58:45 -0400

    properly load/unload resource

--- src/lib/utils/hooks/usePreFetchImage.ts
+++ src/lib/utils/hooks/usePreFetchImage.ts
@@ -2,29 +2,42 @@ import { useEffect, useState } from 'react'
 
 /**
  * Custom hook for retrieving a resource and assigning it a localhost URL. This is useful for
- * fetching resources from URLs that may expire before the resource renders
+ * fetching resources from URLs that may expire before the resource renders.
+ *
+ * When the object is no longer needed, you can release it by calling `releaseURL`. For more info,
+ * see docs on `createObjectURL` (https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL)
  * @param preSignedURL
  * @returns a localhost URL referencing the prefetched resource
  */
 export default function usePreFetchResource(
-  preSignedURL: string,
+  preSignedURL?: string,
 ): string | undefined {
-  const [objectURL, setObjectURL] = useState<string>()
+  const [resourceURL, setResourceURL] = useState<string>()
 
   useEffect(() => {
-    const getData = async () => {
+    const getData = async (url: string) => {
       try {
-        const response = await fetch(preSignedURL)
+        const response = await fetch(url)
         const blob = await response.blob()
-        setObjectURL(URL.createObjectURL(blob))
+        setResourceURL(URL.createObjectURL(blob))
       } catch (e) {
         console.error(
-          `Failed to fetch object with presigned URL ${preSignedURL}. See network log for details`,
+          `Failed to fetch object with presigned URL ${url}. See network log for details`,
         )
       }
     }
-    getData()
+    if (preSignedURL) {
+      getData(preSignedURL)
+    }
   }, [preSignedURL])
 
-  return objectURL
+  useEffect(() => {
+    return () => {
+      if (resourceURL) {
+        URL.revokeObjectURL(resourceURL)
+      }
+    }
+  }, [resourceURL])
+
+  return resourceURL
 }
