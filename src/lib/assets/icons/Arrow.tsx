03fd502e3f28a446bab13d468ff0878ac5c656b0
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-04-19 10:16:38 -0400

    Control arrow direction via prop

--- src/lib/assets/icons/Arrow.tsx
+++ src/lib/assets/icons/Arrow.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
-const Arrow = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    {...props}
-    className={`SRC-arrow-icon ${props.className ?? ''}`}
-    width="35"
-    height="35"
-    viewBox="0 0 35 35"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M0 15.2904L0 19.7096L26.5152 19.7096L14.3624 31.8624L17.5 35L35 17.5L17.5 -1.68579e-07L14.3624 3.13763L26.5152 15.2904L0 15.2904Z" />
-  </svg>
-)
+type ArrowProps = React.SVGProps<SVGSVGElement> & {
+  arrowDirection: 'left' | 'right'
+}
+
+const Arrow = ({ arrowDirection, ...props }: ArrowProps) => {
+  return (
+    <svg
+      {...props}
+      className={`SRC-arrow-icon ${props.className ?? ''}`}
+      style={{
+        ...props.style,
+        transform: `${arrowDirection === 'left' ? 'rotate(180deg)' : ''} ${
+          props.style?.transform ?? ''
+        }`,
+      }}
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 15.2904L0 19.7096L26.5152 19.7096L14.3624 31.8624L17.5 35L35 17.5L17.5 -1.68579e-07L14.3624 3.13763L26.5152 15.2904L0 15.2904Z" />
+    </svg>
+  )
+}
 
 export default Arrow
