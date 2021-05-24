68b389dbfe0a65a3d9414effd9193a5de7adae5f
Jay Hodgson <jay.hodgson@sagebase.org>
2021-05-24 16:00:22 -0700

    fix types

---
+++ src/lib/utils/synapseTypes/AuthenticatedOn.ts
@@ -0,0 +1,5 @@
+// Subset of fields found in https://rest-docs.synapse.org/rest/org/sagebionetworks/repo/model/oauth/OAuthClient.html
+
+export type AuthenticatedOn = {
+  authenticatedOn: string
+}
