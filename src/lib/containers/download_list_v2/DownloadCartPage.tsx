2fe9e5c735c4dbb3dac23292c1640a22e7064a9e
Jay Hodgson <jay.hodgson@sagebase.org>
2021-06-09 15:31:55 -0700

    more progress

--- src/lib/containers/download_list_v2/DownloadCartPage.tsx
+++ src/lib/containers/download_list_v2/DownloadCartPage.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import * as React from 'react'
+import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { useSynapseContext } from '../../utils/SynapseContext'
 import { SynapseErrorBoundary } from '../ErrorBanner'
+import AvailableForDownloadTable from './AvailableForDownloadTable'
 import DownloadListStats from './DownloadListStats'
 
 /**
  * All components wrapped by a query client provider (for caching) and an error boundary.
  */
 export default function DownloadCartPage() {
+  const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0)
   const { accessToken } = useSynapseContext()
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -22,17 +25,36 @@ export default function DownloadCartPage() {
   }
   return (
     <div className="DownloadCartPage bootstrap-4-backport">
+      <h3 className="title">Your Download Cart</h3>
+      <p className="subTitle">You may find your added files in the tabs below. Any files which require actions before download can be found in the Actions Required tab,
+        while any can be downloaded now can be found in the Available for Download tab.</p>
       <QueryClientProvider client={queryClient}>
         <SynapseErrorBoundary>
-          <DownloadListStats />
           <ul className="nav nav-tabs">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">Active</a>
+          <li className={`nav-item ${
+                selectedTabIndex == 0 ? 'active' : ''
+              }`}>
+              <button onClick={() => setSelectedTabIndex(0)}>
+                Access Actions Required
+              </button>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+            <li className={`nav-item ${
+                selectedTabIndex == 1 ? 'active' : ''
+              }`}>
+              <button onClick={() => setSelectedTabIndex(1)}>
+                Download List
+              </button>
             </li>
           </ul>
+          { selectedTabIndex == 0 && <div>
+            <h2>access actions required</h2>
+          </div>
+            }
+          { selectedTabIndex == 1 && <div>
+            <DownloadListStats />
+            <AvailableForDownloadTable /> 
+          </div>
+          }
         </SynapseErrorBoundary>
       </QueryClientProvider>
     </div>
