8159599267f51c80015162301c86a297731d3a9f
Jay Hodgson <jay.hodgson@sagebase.org>
2021-06-09 17:00:55 -0700

    Download List tab looks pretty close to the design now

--- src/lib/containers/download_list_v2/DownloadCartPage.tsx
+++ src/lib/containers/download_list_v2/DownloadCartPage.tsx
@@ -25,34 +25,52 @@ export default function DownloadCartPage() {
   }
   return (
     <div className="DownloadCartPage bootstrap-4-backport">
-      <h3 className="title">Your Download Cart</h3>
-      <p className="subTitle">You may find your added files in the tabs below. Any files which require actions before download can be found in the Actions Required tab,
-        while any can be downloaded now can be found in the Available for Download tab.</p>
+      <div className="container">
+        <h3 className="title">Your Download Cart</h3>
+        <p className="description">You may find your added files in the tabs below. Any files which require actions before download can be found in the Actions Required tab,
+          while any can be downloaded now can be found in the Download List tab.</p>
+      </div>
       <QueryClientProvider client={queryClient}>
         <SynapseErrorBoundary>
-          <ul className="nav nav-tabs">
-          <li className={`nav-item ${
-                selectedTabIndex == 0 ? 'active' : ''
-              }`}>
-              <button onClick={() => setSelectedTabIndex(0)}>
-                Access Actions Required
-              </button>
-            </li>
-            <li className={`nav-item ${
-                selectedTabIndex == 1 ? 'active' : ''
-              }`}>
-              <button onClick={() => setSelectedTabIndex(1)}>
-                Download List
-              </button>
-            </li>
-          </ul>
+          <div className="tabs-container">
+            <div className="container">
+              <ul className="nav nav-tabs">
+                <li className={`nav-item ${
+                    selectedTabIndex == 0 ? 'active' : ''
+                  }`}>
+                  <button onClick={() => setSelectedTabIndex(0)}>
+                    Access Actions Required
+                  </button>
+                </li>
+                <li className={`nav-item ${
+                    selectedTabIndex == 1 ? 'active' : ''
+                  }`}>
+                  <button onClick={() => setSelectedTabIndex(1)}>
+                    Download List
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
           { selectedTabIndex == 0 && <div>
             <h2>access actions required</h2>
           </div>
             }
           { selectedTabIndex == 1 && <div>
-            <DownloadListStats />
-            <AvailableForDownloadTable /> 
+            <div className="availableForDownloadOverview">
+              <div className="container">
+                <div className="completeYourDownloadContainer">
+                  <span className="completeYourDownload">Complete Your Download</span>
+                  <DownloadListStats />
+                </div>
+                <p className="description">Downloading your files programmatically is the quickest and most efficient way to get all of your files, 
+                both internal and externally hosted. Metadata will always be included in your download automatically when downloading programmatically. 
+                If you choose to download as .zip files, you can download external files individually at any time.</p>
+              </div>
+            </div>
+            <div className="availableForDownloadTableContainer container">
+              <AvailableForDownloadTable /> 
+            </div>
           </div>
           }
         </SynapseErrorBoundary>
