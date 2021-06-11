1c214145f632c16e065abdd9c31c387f9cb99676
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-06-11 11:10:19 -0400

    Refactor UserCardMedium to be a function component and improve test stability

--- src/mocks/msw/handlers.ts
+++ src/mocks/msw/handlers.ts
@@ -10,7 +10,7 @@ import {
   MOCK_FILE_ENTITY_ID,
   MOCK_PROJECT_ID,
 } from '../entity/mockEntity'
-import { mockUserProfileData, MOCK_USER_ID } from '../user/mock_user_profile'
+import { mockUserBundle, mockUserProfileData, MOCK_USER_ID } from '../user/mock_user_profile'
 
 const handlers = [
   rest.options('*', async (req, res, ctx) => {
@@ -25,6 +25,19 @@ const handlers = [
       let response
       if (req.params.id === MOCK_USER_ID.toString()) {
         response = mockUserProfileData
+      }
+      return res(ctx.status(200), ctx.json(response))
+    },
+  ),
+
+  rest.get(
+    `${getEndpoint(
+      BackendDestinationEnum.REPO_ENDPOINT,
+    )}repo/v1/user/:id/bundle`,
+    async (req, res, ctx) => {
+      let response
+      if (req.params.id === MOCK_USER_ID.toString()) {
+        response = mockUserBundle
       }
       return res(ctx.status(200), ctx.json(response))
     },
