689c73cf137d89504606f8ffd14498f2f20a844d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-06-14 13:48:00 -0400

    SWC-5620 - Entity Metadata Modal

---
+++ src/__tests__/lib/containers/entity/metadata/AnnotationsTable.test.tsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import {
+  AnnotationsTable,
+  AnnotationsTableProps,
+} from '../../../../../lib/containers/entity/metadata/AnnotationsTable'
+import { SynapseContextType } from '../../../../../lib/utils/SynapseContext'
+import {
+  mockFileEntityBundle,
+  MOCK_FILE_ENTITY_ID,
+} from '../../../../../mocks/entity/mockEntity'
+import { MOCK_CONTEXT_VALUE } from '../../../../../mocks/MockSynapseContext'
+import { rest, server } from '../../../../../mocks/msw/server'
+import { mockSchemaBinding } from '../../../../../mocks/mockSchema'
+import {
+  BackendDestinationEnum,
+  getEndpoint,
+} from '../../../../../lib/utils/functions/getEndpoint'
+import { createWrapper } from '../../../../../lib/testutils/TestingLibraryUtils'
+
+const defaultProps: AnnotationsTableProps = {
+  entityId: MOCK_FILE_ENTITY_ID,
+}
+
+function renderComponent(wrapperProps?: SynapseContextType) {
+  render(<AnnotationsTable {...defaultProps} />, {
+    wrapper: createWrapper(wrapperProps),
+  })
+}
+
+describe('AnnotationsTable tests', () => {
+  // Handle the msw lifecycle:
+  beforeAll(() => server.listen())
+  afterEach(() => server.restoreHandlers())
+  afterAll(() => server.close())
+
+  it('Renders the annotations on an entity', async () => {
+    renderComponent()
+    await screen.findByText('myStringKey')
+    for (const key of Object.keys(
+      mockFileEntityBundle.annotations!.annotations,
+    )) {
+      expect(() => screen.getByText(key)).not.toThrowError()
+      screen.getByText(
+        mockFileEntityBundle.annotations!.annotations[key].value.join(', '),
+      )
+    }
+  })
+
+  it('Displays the validation schema if there is one (in experimental mode)', async () => {
+    renderComponent({
+      ...MOCK_CONTEXT_VALUE,
+      isInExperimentalMode: true,
+    })
+
+    await screen.findByText('Validation Schema')
+    await screen.findByText(mockSchemaBinding.jsonSchemaVersionInfo.schemaName)
+  })
+
+  it('Displays a placeholder when annotations is undefined', async () => {
+    server.use(
+      rest.post(
+        `${getEndpoint(
+          BackendDestinationEnum.REPO_ENDPOINT,
+        )}repo/v1/entity/:entityId/bundle2`,
+
+        async (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({ ...mockFileEntityBundle, annotations: undefined }),
+          )
+        },
+      ),
+    )
+    renderComponent()
+    await screen.findByText('This File has no annotations.')
+  })
+
+  it('Displays a placeholder when annotations is empty', async () => {
+    server.use(
+      rest.post(
+        `${getEndpoint(
+          BackendDestinationEnum.REPO_ENDPOINT,
+        )}repo/v1/entity/:entityId/bundle2`,
+
+        async (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              ...mockFileEntityBundle,
+              annotations: { id: 'myId', etag: 'etag', annotations: {} },
+            }),
+          )
+        },
+      ),
+    )
+    renderComponent()
+    await screen.findByText('This File has no annotations.')
+  })
+})
---
+++ src/lib/containers/entity/metadata/EntityModal.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import { entityTypeToFriendlyName } from '../../../utils/functions/EntityTypeUtils'
+import {
+  BackendDestinationEnum,
+  getEndpoint,
+} from '../../../utils/functions/getEndpoint'
+import useGetEntityBundle from '../../../utils/hooks/SynapseAPI/useEntityBundle'
+import { SynapseSpinner } from '../../LoadingScreen'
+import { AnnotationsTable } from './AnnotationsTable'
+import { MetadataTable } from './MetadataTable'
+
+enum EntityModalTabs {
+  METADATA = 'METADATA', // non-annotation metadata about the entity
+  ANNOTATIONS = 'ANNOTATIONS', // annotation and schema information
+  // TODO: Access -- we haven't yet built a viewer/editor for ACLs in SRC -- consider a redesign before building
+  // ACCESS = 'ACCESS', // ACLs (and maybe ARs?)
+  // TODO: Previews - we would need preview renderers that accomplish feature parity with SWC
+  // PREVIEW = 'PREVIEW' // should only show this tab if a preview exists
+}
+
+export type EntityModalProps = {
+  readonly show: boolean
+  readonly entityId: string
+  readonly onClose: () => void
+  readonly initialTab?: EntityModalTabs
+}
+
+export const EntityModal: React.FC<EntityModalProps> = ({
+  entityId,
+  show,
+  onClose,
+  initialTab = EntityModalTabs.METADATA,
+}: EntityModalProps) => {
+  const [currentTab, setCurrentTab] = useState<EntityModalTabs>(initialTab)
+  const { data: entityBundle, isLoading: isLoadingBundle } = useGetEntityBundle(
+    entityId,
+  )
+
+  return (
+    <Modal
+      className="bootstrap-4-backport EntityMetadata"
+      show={show}
+      animation={false}
+      onClose={onClose}
+    >
+      {isLoadingBundle ? (
+        <SynapseSpinner />
+      ) : (
+        <>
+          <Modal.Header closeButton onHide={onClose}>
+            <Modal.Title>{entityBundle?.entity?.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="Tabs">
+              {Object.keys(EntityModalTabs).map((tabName: string) => {
+                return (
+                  <div
+                    className="Tab"
+                    key={tabName}
+                    onClick={() => setCurrentTab(EntityModalTabs[tabName])}
+                    aria-selected={tabName === currentTab}
+                  >
+                    {tabName}
+                  </div>
+                )
+              })}
+            </div>
+            {!isLoadingBundle && currentTab === EntityModalTabs.ANNOTATIONS && (
+              <AnnotationsTable entityId={entityId} />
+            )}
+            {!isLoadingBundle && currentTab === EntityModalTabs.METADATA && (
+              <MetadataTable entityId={entityId} />
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <div style={{ display: 'flex', width: '100%' }}>
+              <div style={{ flexGrow: 1 }}></div>
+              {/* {!isLoadingBundle &&
+                currentTab === EntityModalTabs.ANNOTATIONS && (
+                  <AnnotationsSummaryFooter
+                    entityBundle={entityBundle!}
+                    onClose={onClose}
+                  />
+                )}
+              {!isLoadingBundle &&
+                currentTab === EntityModalTabs.METADATA && ( */}
+              <Button
+                variant="primary"
+                onClick={() =>
+                  window.open(
+                    `${getEndpoint(
+                      BackendDestinationEnum.PORTAL_ENDPOINT,
+                    )}#!Synapse:${entityBundle!.entity!.id}`,
+                    '_blank',
+                    'noopener',
+                  )
+                }
+              >
+                Open {entityTypeToFriendlyName(entityBundle!.entityType!)}
+              </Button>
+              {/* )} */}
+            </div>
+          </Modal.Footer>
+        </>
+      )}
+    </Modal>
+  )
+}
