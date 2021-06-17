8ef4d394b518a2e69434fa1e556d16439ec46e91
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-06-17 11:53:31 -0400

    Show schema even if there are no annotations

--- src/lib/containers/entity/metadata/AnnotationsTable.tsx
+++ src/lib/containers/entity/metadata/AnnotationsTable.tsx
@@ -34,49 +34,47 @@ export const AnnotationsTable: React.FC<AnnotationsTableProps> = ({
           This {entityTypeToFriendlyName(entityBundle.entityType!)} has no
           annotations.
         </div>
-      ) : (
-        <table className="AnnotationsTable">
-          <tbody>
-            {Object.keys(entityBundle.annotations!.annotations).map(
-              (key: string) => {
-                return (
-                  <tr key={key} className="AnnotationsTable__Row">
-                    <td className="AnnotationsTable__Row__Key">{key}</td>
-                    <td className="AnnotationsTable__Row__Value">
-                      {entityBundle.annotations?.annotations[key].value
-                        ? entityBundle.annotations?.annotations[key].value
-                            .map((v: string | number) => v?.toString())
-                            .join(', ')
-                        : null}
-                    </td>
-                  </tr>
-                )
-              },
-            )}
-            {boundSchema && isInExperimentalMode ? (
-              <tr className="AnnotationsTable__Row">
-                <td className="AnnotationsTable__Row__Key Schema">
-                  Validation Schema
-                </td>
-                <td className="AnnotationsTable__Row__Value">
-                  <a
-                    href={`${getEndpoint(
-                      BackendDestinationEnum.REPO_ENDPOINT,
-                    )}repo/v1/schema/type/registered/${
-                      boundSchema.jsonSchemaVersionInfo.$id
-                    }`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {boundSchema.jsonSchemaVersionInfo.schemaName}
-                  </a>
-                </td>
-              </tr>
-            ) : null}
-          </tbody>
-        </table>
-      )}
+      ) : null}
+      <table className="AnnotationsTable">
+        <tbody>
+          {Object.keys(entityBundle.annotations!.annotations).map(
+            (key: string) => {
+              return (
+                <tr key={key} className="AnnotationsTable__Row">
+                  <td className="AnnotationsTable__Row__Key">{key}</td>
+                  <td className="AnnotationsTable__Row__Value">
+                    {entityBundle.annotations?.annotations[key].value
+                      ? entityBundle.annotations?.annotations[key].value.join(
+                          ', ',
+                        )
+                      : null}
+                  </td>
+                </tr>
+              )
+            },
+          )}
+          {boundSchema && isInExperimentalMode ? (
+            <tr className="AnnotationsTable__Row">
+              <td className="AnnotationsTable__Row__Key Schema">
+                Validation Schema
+              </td>
+              <td className="AnnotationsTable__Row__Value">
+                <a
+                  href={`${getEndpoint(
+                    BackendDestinationEnum.REPO_ENDPOINT,
+                  )}repo/v1/schema/type/registered/${
+                    boundSchema.jsonSchemaVersionInfo.$id
+                  }`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {boundSchema.jsonSchemaVersionInfo.schemaName}
+                </a>
+              </td>
+            </tr>
+          ) : null}
+        </tbody>
+      </table>
     </>
   ) : null
 }
-
