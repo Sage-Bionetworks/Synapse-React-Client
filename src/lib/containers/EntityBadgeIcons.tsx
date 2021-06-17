74d1e2f86f46e273f3c33f908a123b8227c45cab
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-06-17 11:54:25 -0400

    Get bundle in the badge

--- src/lib/containers/EntityBadgeIcons.tsx
+++ src/lib/containers/EntityBadgeIcons.tsx
@@ -26,6 +26,7 @@ import {
 } from '../utils/hooks/SynapseAPI/useSchema'
 import { useEffect } from 'react'
 import { EntityModal, EntityModalTabs } from './entity/metadata/EntityModal'
+import useGetEntityBundle from '../utils/hooks/SynapseAPI/useEntityBundle'
 
 const isPublic = (bundle: EntityBundle): boolean => {
   return bundle.benefactorAcl!.resourceAccess.some(ra => {
@@ -39,7 +40,6 @@ const isPublic = (bundle: EntityBundle): boolean => {
 
 type EntityBadgeIconsProps = {
   entityId: string
-  bundle: EntityBundle
   flexWrap?: // possible settings for flex-wrap
   | 'wrap'
     | 'nowrap'
@@ -50,31 +50,43 @@ type EntityBadgeIconsProps = {
     | 'unset'
     | 'wrap-reverse'
   justifyContent?: 'flex-start' | 'flex-end' | string
-  /* shows an 'unlink' button if the entity is a link and the user has permission to delete it */
-  showUnlink?: false
-  /* invoked after the entity is unlinked/deleted in case there is cleanup to do */
-  onUnlink?: (entityId?: string) => void
+  /** Shows an icon indicating if the entity is 'public' or 'private'. Default true  */
+  showIsPublicPrivate?: boolean
+  /** Shows an icon if the entity has sharing settings set on itself. Default true  */
+  showHasLocalSharingSettings?: boolean
+  /** Shows an icon if the entity has annotations, or if it has a validation schema (in experimental mode only). Default true  */
+  showHasAnnotations?: boolean
+  /** Shows an icon if the entity a wiki. Default true  */
+  showHasWiki?: boolean
+  /** Shows an icon if the entity has been mentioned in discussion threads. Default true  */
+  showHasDiscussionThread?: boolean
+  /* Shows an 'unlink' button if the entity is a link and the user has permission to delete it. Default true */
+  showUnlink?: boolean
+  /* Invoked after the entity is unlinked/deleted in case there is cleanup to do. Returns the entityId */
+  onUnlink?: (entityId: string) => void
+  onUnlinkErr?: (error: Error) => void
 }
 
 /**
  * Stateless component used to show multiple entity badges.
  * Adapted from https://github.com/Sage-Bionetworks/SynapseWebClient/blob/46b9b717636cda2421926d96365244bbb72a05b6/src/main/java/org/sagebionetworks/web/client/widget/entity/EntityBadge.java
- *
- * List of badges and bundle components required to show them:
- * - Private/Public badge (benefactorAcl)
- * - Has Local Sharing Settings (benefactorAcl)
- * - Annotations badge (annotations)
- * - Has Wiki (rootWikiId)
- * - Has Discussion Thread (threadCount)
- * - Unlink (requires entityType, permission to delete, and showUnlink to be true)
  */
 export const EntityBadgeIcons: React.FunctionComponent<EntityBadgeIconsProps> = ({
   entityId,
-  bundle,
   flexWrap = 'nowrap',
   justifyContent = 'flex-start',
-  showUnlink,
-  onUnlink,
+  showIsPublicPrivate = true,
+  showHasLocalSharingSettings = true,
+  showHasAnnotations = true,
+  showHasWiki = true,
+  showHasDiscussionThread = true,
+  showUnlink = true,
+  onUnlink = () => {
+    /* noop */
+  },
+  onUnlinkErr = () => {
+    /* noop */
+  },
 }) => {
   const ENTITY_BADGE_ICONS_TOOLTIP_ID = 'EntityBadgeIconsTooltipID-' + entityId
 
@@ -85,6 +97,7 @@ export const EntityBadgeIcons: React.FunctionComponent<EntityBadgeIconsProps> = 
     ANNOTATIONS_MISSING = 'Incomplete',
   }
 
+  const { data: bundle } = useGetEntityBundle(entityId)
   const [showModal, setShowModal] = useState(false)
   const [schemaConformance, setSchemaConformance] = useState(
     SchemaConformanceState.NO_SCHEMA,
@@ -102,48 +115,9 @@ export const EntityBadgeIcons: React.FunctionComponent<EntityBadgeIconsProps> = 
   // The maximum number of annotations to show in the popover
   const maxAnnosToShow = 10
   const annotationsCount =
-    bundle.annotations && !isEmpty(bundle.annotations.annotations)
+    bundle?.annotations && !isEmpty(bundle.annotations.annotations)
       ? Object.keys(bundle.annotations.annotations).length
       : 0
-  /**
-   * Convert the list of annotations to a string of <tr>...anno1</tr><tr>...anno2</tr>...
-   * If there are no annotations, this is the empty string ('')
-   */
-  const annotationsTableRows = `${Object.entries(
-    bundle.annotations!.annotations,
-  ).reduce((previous, current, index) => {
-    if (
-      index < maxAnnosToShow ||
-      (index === maxAnnosToShow && maxAnnosToShow === annotationsCount)
-    ) {
-      return `${previous}<tr><td><b>${
-        current[0]
-      }</b></td><td>${current[1].value.join(', ')}</td></tr>`
-    } else {
-      return previous
-    }
-  }, '')}`
-
-  // We also show the schema name if there is one (and we're in experimental mode)
-  const valiationSchemaTableRow =
-    isInExperimentalMode && boundSchema
-      ? `<tr><td><b>Validation Schema</b></td><td>${boundSchema.jsonSchemaVersionInfo.schemaName}</td></tr>`
-      : ''
-
-  // Format it all as an html table
-  const annotationsHtml = `<table>
-      ${annotationsTableRows ? annotationsTableRows : 'No annotations'}
-      ${valiationSchemaTableRow}
-      </table>${
-        annotationsCount > maxAnnosToShow
-          ? `<p>and ${annotationsCount - maxAnnosToShow} more</p>`
-          : ''
-      }`
-
-  if (entityId === 'syn25741220') {
-    console.log(annotationsCount)
-    console.log(annotationsHtml)
-  }
 
   useEffect(() => {
     if (isInExperimentalMode && schemaValidationResults) {
@@ -170,6 +144,48 @@ export const EntityBadgeIcons: React.FunctionComponent<EntityBadgeIconsProps> = 
     SchemaConformanceState.ANNOTATIONS_MISSING,
   ])
 
+  /**
+   * Convert the list of annotations to a string of <tr>...anno1</tr><tr>...anno2</tr>...
+   * If there are no annotations, this is the empty string ('')
+   */
+  const annotationsTableRows = bundle
+    ? `${Object.entries(bundle.annotations!.annotations).reduce(
+        (previous, current, index) => {
+          if (
+            index < maxAnnosToShow ||
+            (index === maxAnnosToShow && maxAnnosToShow === annotationsCount)
+          ) {
+            return `${previous}<tr><td><b>${
+              current[0]
+            }</b></td><td>${current[1].value.join(', ')}</td></tr>`
+          } else {
+            return previous
+          }
+        },
+        '',
+      )}`
+    : ''
+
+  // We also show the schema name if there is one (and we're in experimental mode)
+  const valiationSchemaTableRow =
+    isInExperimentalMode && boundSchema
+      ? `<tr><td><b>Validation Schema</b></td><td>${boundSchema.jsonSchemaVersionInfo.schemaName}</td></tr>`
+      : ''
+
+  // Format it all as an html table
+  const annotationsHtml = `<table>
+      ${annotationsTableRows ? annotationsTableRows : 'No annotations'}
+      ${valiationSchemaTableRow}
+      </table>${
+        annotationsCount > maxAnnosToShow
+          ? `<p>and ${annotationsCount - maxAnnosToShow} more</p>`
+          : ''
+      }`
+
+  if (bundle == null) {
+    return null
+  }
+
   return (
     <div
       className="EntityBadge"
@@ -188,44 +204,44 @@ export const EntityBadgeIcons: React.FunctionComponent<EntityBadgeIconsProps> = 
         onClose={() => setShowModal(false)}
         initialTab={EntityModalTabs.ANNOTATIONS}
       />
-      {bundle.benefactorAcl && (
-        <>
-          {isPublic(bundle) ? (
-            <FontAwesomeIcon
-              className="EntityBadge__Badge"
-              icon={faGlobeAmericas}
-              data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
-              data-tip={'Public'}
-            />
-          ) : (
-            <FontAwesomeIcon
-              className="EntityBadge__Badge"
-              icon={faLock}
-              data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
-              data-tip={'Private'}
-            />
-          )}
-          {entityId === bundle.benefactorAcl.id && (
-            <FontAwesomeIcon
-              className="EntityBadge__Badge"
-              icon={faCheck}
-              data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
-              data-tip="Sharing Settings have been set"
-            />
-          )}
-        </>
-      )}
-      {!!(annotationsCount || schemaValidationResults) && (
+
+      {showIsPublicPrivate && isPublic(bundle) ? (
         <FontAwesomeIcon
-          className={`EntityBadge__Badge Annotations ${schemaConformance}`}
-          onClick={() => setShowModal(true)}
-          icon={faTag}
+          className="EntityBadge__Badge"
+          icon={faGlobeAmericas}
           data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
-          data-tip={sanitizeHtml(annotationsHtml)}
-          data-html={true}
+          data-tip={'Public'}
         />
-      )}
-      {bundle.rootWikiId && (
+      ) : null}
+      {showIsPublicPrivate && !isPublic(bundle) ? (
+        <FontAwesomeIcon
+          className="EntityBadge__Badge"
+          icon={faLock}
+          data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
+          data-tip={'Private'}
+        />
+      ) : null}
+      {showHasLocalSharingSettings && entityId === bundle.benefactorAcl!.id ? (
+        <FontAwesomeIcon
+          className="EntityBadge__Badge"
+          icon={faCheck}
+          data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
+          data-tip="Sharing Settings have been set"
+        />
+      ) : null}
+
+      {showHasAnnotations &&
+        !!(annotationsCount || schemaValidationResults) && (
+          <FontAwesomeIcon
+            className={`EntityBadge__Badge Annotations ${schemaConformance}`}
+            onClick={() => setShowModal(true)}
+            icon={faTag}
+            data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
+            data-tip={sanitizeHtml(annotationsHtml)}
+            data-html={true}
+          />
+        )}
+      {showHasWiki && bundle.rootWikiId && (
         <FontAwesomeIcon
           //   style={{ maxWidth: '20px', maxHeight: '20px' }}
           className="EntityBadge__Badge"
@@ -234,23 +250,24 @@ export const EntityBadgeIcons: React.FunctionComponent<EntityBadgeIconsProps> = 
           data-tip="Has a wiki"
         />
       )}
-      {!!bundle.threadCount && !!(bundle.threadCount > 0) && (
-        <FontAwesomeIcon
-          className="EntityBadge__Badge"
-          icon={faComment}
-          data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
-          data-tip="Has been mentioned in discussion"
-        />
-      )}
+      {showHasDiscussionThread &&
+        !!bundle.threadCount &&
+        !!(bundle.threadCount > 0) && (
+          <FontAwesomeIcon
+            className="EntityBadge__Badge"
+            icon={faComment}
+            data-for={ENTITY_BADGE_ICONS_TOOLTIP_ID}
+            data-tip="Has been mentioned in discussion"
+          />
+        )}
       {showUnlink &&
         bundle.entityType === EntityType.LINK &&
         bundle.permissions?.canDelete && (
           <FontAwesomeIcon
-            onClick={async () => {
-              await SynapseClient.deleteEntity(accessToken, entityId)
-              if (onUnlink) {
-                onUnlink(entityId)
-              }
+            onClick={() => {
+              SynapseClient.deleteEntity(accessToken, entityId)
+                .then(() => onUnlink(entityId))
+                .catch(error => onUnlinkErr(error))
             }}
             className="EntityBadge__Badge Unlink"
             icon={faUnlink}
