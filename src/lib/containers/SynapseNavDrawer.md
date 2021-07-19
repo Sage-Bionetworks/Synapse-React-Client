8e77e16253ef198782e79a41a9f9ce4cd3c1b510
Jay Hodgson <jay.hodgson@sagebase.org>
2021-07-19 16:22:15 -0700

    prototype in progress

---
+++ src/lib/containers/SynapseNavDrawer.md
@@ -0,0 +1,6 @@
+```jsx
+// <SynapseNavDrawer initIsOpen={false}/>
+<div>
+  <p>As soon as this component is rendered on the screen, the nav bar will be visible on the left.</p>
+</div>
+```
---
+++ src/lib/containers/SynapseNavDrawer.tsx
@@ -0,0 +1,63 @@
+import { Drawer, IconButton, List, ListItem } from '@material-ui/core'
+import React, {useState} from 'react'
+import IconSvg from './IconSvg'
+
+export type SynapseNavDrawerProps = {
+  initIsOpen?: boolean
+}
+
+export enum NavItem {
+  PROJECTS,
+  FAVORITES,
+  TEAMS,
+  CHALLENGES,
+  DOWNLOADS,
+  SEARCH,
+  PROFILE,
+  HELP
+}
+
+/**
+ * Displays the Synapse navigational drawer on the left side of the page.  Has links to various areas if logged in.
+ */
+export const SynapseNavDrawer: React.FunctionComponent<SynapseNavDrawerProps> = ({initIsOpen = false}) => {
+  const [isOpen, setOpen] = useState(initIsOpen)
+  const [selectedItem, setSelectedItem] = useState<NavItem>()
+  const handleDrawerOpen = (navItem?:NavItem) => {
+    setOpen(true)
+    setSelectedItem(navItem)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+    setSelectedItem(undefined)
+  }
+  return (
+    <div className="SynapseNavDrawer">
+      <Drawer variant="permanent" className={`SynapseNavDrawerMenu ${isOpen ? 'drawerOpen' : 'drawerClosed'}`} onClose={handleDrawerClose} >
+          <List>
+            <ListItem button key='synapse' onClick={() => {handleDrawerOpen(undefined)}}>
+              <IconSvg options={{ icon: 'download' }} />
+            </ListItem>
+            <ListItem button key='projects' onClick={() => {handleDrawerOpen(NavItem.PROJECTS)}}>
+              <IconSvg options={{ icon: 'download' }} />
+            </ListItem>
+            <ListItem button key='favorites' onClick={() => {handleDrawerOpen(NavItem.FAVORITES)}}>
+              <IconSvg options={{ icon: 'download' }} />
+            </ListItem>
+          </List>
+      </Drawer>
+      <div className="SynapseNavDrawerSelectedContent">
+        {selectedItem == NavItem.PROJECTS && <>
+            <div>
+              SYNAPSE
+              PROJECTS UI<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </>}
+          {selectedItem == NavItem.FAVORITES && <>
+            <div>FAVORITES UI<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+          </>}
+      </div>
+    </div>
+  )
+}
