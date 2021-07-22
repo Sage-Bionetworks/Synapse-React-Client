67895dd7525d2ba26c08adfbf11ab89a5f127de3
Jay Hodgson <jay.hodgson@sagebase.org>
2021-07-21 17:06:42 -0700

    with additional links, and a temp slide out drawer that can be closed by clicking on the backdrop

--- src/lib/containers/SynapseNavDrawer.tsx
+++ src/lib/containers/SynapseNavDrawer.tsx
@@ -1,5 +1,5 @@
 import { Drawer, List, ListItem } from '@material-ui/core'
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import IconSvg from './IconSvg'
 import SynapseIcon from '../assets/icons/SynapseIcon'
 import ReactTooltip from 'react-tooltip'
@@ -26,7 +26,7 @@ export enum NavItem {
 /**
  * Displays the Synapse navigational drawer on the left side of the page.  Has links to various areas if logged in.
  */
-export const SynapseNavDrawer: React.FunctionComponent<SynapseNavDrawerProps> = ({initIsOpen = false}) => {
+export const SynapseNavDrawer: React.FunctionComponent<SynapseNavDrawerProps> = ({ initIsOpen = false }) => {
   const { accessToken } = useSynapseContext()
   const [isOpen, setOpen] = useState(initIsOpen)
   const [selectedItem, setSelectedItem] = useState<NavItem>()
@@ -37,7 +37,7 @@ export const SynapseNavDrawer: React.FunctionComponent<SynapseNavDrawerProps> = 
       const userProfile = await SynapseClient.getUserProfile(accessToken)
       setUserProfile(userProfile)
     }
-    if(accessToken) {
+    if (accessToken) {
       getUserProfile()
     } else {
       setUserProfile(undefined)
@@ -45,7 +45,7 @@ export const SynapseNavDrawer: React.FunctionComponent<SynapseNavDrawerProps> = 
   }, [
     accessToken
   ])
-  const handleDrawerOpen = (navItem?:NavItem) => {
+  const handleDrawerOpen = (navItem?: NavItem) => {
     setOpen(true)
     setSelectedItem(navItem)
   }
@@ -54,114 +54,119 @@ export const SynapseNavDrawer: React.FunctionComponent<SynapseNavDrawerProps> = 
     setOpen(false)
     setSelectedItem(undefined)
   }
-  
+
   return (
     <div className="SynapseNavDrawer">
-      <Drawer variant="permanent" className={`SynapseNavDrawerMenu 'drawerClosed`} >
-          <a href='#!Home:0' className='synapseIcon' rel="noopener noreferrer">
-            <SynapseIcon />
-          </a>
-          <List>
-            {currentUserProfile && <ListItem button key='projects' data-tip="Projects" data-for="projectLink" onClick={selectedItem == NavItem.PROJECTS  ? handleDrawerClose : () => {handleDrawerOpen(NavItem.PROJECTS)}}>
-              <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="projectLink"
-                />
-              <IconSvg options={{ icon: 'dashboard' }} />
-            </ListItem>}
-            {currentUserProfile && <ListItem button key='favorites' data-tip="Favorites" data-for="favsLink" onClick={selectedItem == NavItem.FAVORITES ? handleDrawerClose : () => {handleDrawerOpen(NavItem.FAVORITES)}}>
-              <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="favsLink"
-                />
-              <a href={`#!Profile:${currentUserProfile.ownerId}/favorites`} rel="noopener noreferrer">
-                <IconSvg options={{ icon: 'favOutlined' }} />
-              </a>
-            </ListItem>}
-            {currentUserProfile && <ListItem button key='teams' data-tip="Teams" data-for="teamsLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => {handleDrawerOpen(NavItem.TEAMS)}}>
-              <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="teamsLink"
-                />
-              <a href={`#!Profile:${currentUserProfile.ownerId}/teams`} rel="noopener noreferrer">
-                <IconSvg options={{ icon: 'peopleOutlined' }} />
-              </a>
-            </ListItem>}
-            {currentUserProfile && <ListItem button key='challenges' data-tip="Challenges" data-for="challengeLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => {handleDrawerOpen(NavItem.TEAMS)}}>
-              <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="challengeLink"
-                />
-                <a href={`#!Profile:${currentUserProfile.ownerId}/challenges`} rel="noopener noreferrer">
-                  <IconSvg options={{ icon: 'challengesOutlined' }} />
-                </a>
-            </ListItem>}
-            {currentUserProfile && <ListItem button key='download' data-tip="Download Cart" data-for="downloadLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => {handleDrawerOpen(NavItem.TEAMS)}}>
-              <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="downloadLink"
-                />
-              <a href={`#!Profile:${currentUserProfile.ownerId}/downloads`} rel="noopener noreferrer">
-                <IconSvg options={{ icon: 'downloadOutlined' }} />
-              </a>
-            </ListItem>}
-            <ListItem button key='search' data-tip="Search" data-for="searchLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => {handleDrawerOpen(NavItem.TEAMS)}}>
-             <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="searchLink"
-                />
+      <Drawer variant="permanent" className='SynapseNavDrawerMenu' >
+        <a href='#!Home:0' className='synapseIcon' rel="noopener noreferrer">
+          <SynapseIcon />
+        </a>
+        <List>
+          {currentUserProfile && <ListItem button key='projects' data-tip="Projects" data-for="projectLink" onClick={selectedItem == NavItem.PROJECTS ? handleDrawerClose : () => { handleDrawerOpen(NavItem.PROJECTS) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="projectLink"
+            />
+            <IconSvg options={{ icon: 'dashboard' }} />
+          </ListItem>}
+          {currentUserProfile && <ListItem button key='favorites' data-tip="Favorites" data-for="favsLink" onClick={selectedItem == NavItem.FAVORITES ? handleDrawerClose : () => { handleDrawerOpen(NavItem.FAVORITES) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="favsLink"
+            />
+            <a href={`#!Profile:${currentUserProfile.ownerId}/favorites`} rel="noopener noreferrer">
+              <IconSvg options={{ icon: 'favOutlined' }} />
+            </a>
+          </ListItem>}
+          {currentUserProfile && <ListItem button key='teams' data-tip="Teams" data-for="teamsLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => { handleDrawerOpen(NavItem.TEAMS) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="teamsLink"
+            />
+            <a href={`#!Profile:${currentUserProfile.ownerId}/teams`} rel="noopener noreferrer">
+              <IconSvg options={{ icon: 'peopleOutlined' }} />
+            </a>
+          </ListItem>}
+          {currentUserProfile && <ListItem button key='challenges' data-tip="Challenges" data-for="challengeLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => { handleDrawerOpen(NavItem.TEAMS) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="challengeLink"
+            />
+            <a href={`#!Profile:${currentUserProfile.ownerId}/challenges`} rel="noopener noreferrer">
+              <IconSvg options={{ icon: 'challengesOutlined' }} />
+            </a>
+          </ListItem>}
+          {currentUserProfile && <ListItem button key='download' data-tip="Download Cart" data-for="downloadLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => { handleDrawerOpen(NavItem.TEAMS) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="downloadLink"
+            />
+            <a href={`#!Profile:${currentUserProfile.ownerId}/downloads`} rel="noopener noreferrer">
+              <IconSvg options={{ icon: 'downloadOutlined' }} />
+            </a>
+          </ListItem>}
+          <ListItem button key='search' data-tip="Search" data-for="searchLink" onClick={selectedItem == NavItem.TEAMS ? handleDrawerClose : () => { handleDrawerOpen(NavItem.TEAMS) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="searchLink"
+            />
+            <a href='#!Search:' rel="noopener noreferrer">
               <IconSvg options={{ icon: 'searchOutlined' }} />
-            </ListItem>
-            {currentUserProfile && <ListItem className='profileItem' button key='profile' data-tip="Your Account" data-for="profileLink" onClick={selectedItem == NavItem.PROFILE ? handleDrawerClose : () => {handleDrawerOpen(NavItem.PROFILE)}}>
-             <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="profileLink"
-                />
-              <Avatar userProfile={currentUserProfile} avatarSize='SMALL'/>
-            </ListItem>}
-            <ListItem className='helpItem' button key='help' data-tip="Help" data-for="helpLink" onClick={selectedItem == NavItem.PROFILE ? handleDrawerClose : () => {handleDrawerOpen(NavItem.PROFILE)}}>
-              <ReactTooltip
-                  delayShow={300}
-                  place="right"
-                  type="dark"
-                  effect="solid"
-                  id="helpLink"
-                />
-              <IconSvg options={{ icon: 'helpOutlined' }} />
-            </ListItem>
-          </List>
+            </a>
+          </ListItem>
+        </List>
+        <div className="filler"/>
+        <List>
+          {currentUserProfile && <ListItem button key='profile' data-tip="Your Account" data-for="profileLink" onClick={selectedItem == NavItem.PROFILE ? handleDrawerClose : () => { handleDrawerOpen(NavItem.PROFILE) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="profileLink"
+            />
+            <Avatar userProfile={currentUserProfile} avatarSize='SMALL' />
+          </ListItem>}
+          <ListItem button key='help' data-tip="Help" data-for="helpLink" onClick={selectedItem == NavItem.PROFILE ? handleDrawerClose : () => { handleDrawerOpen(NavItem.PROFILE) }}>
+            <ReactTooltip
+              delayShow={300}
+              place="right"
+              type="dark"
+              effect="solid"
+              id="helpLink"
+            />
+            <IconSvg options={{ icon: 'helpOutlined' }} />
+          </ListItem>
+        </List>
       </Drawer>
-      <Drawer variant="permanent" open={isOpen} className={`SynapseNavContentDrawer`} onClose={handleDrawerClose} >
+      <Drawer variant="temporary" open={isOpen} className={`SynapseNavContentDrawer`} onClose={handleDrawerClose} >
         {selectedItem == NavItem.PROJECTS && <>
-            <div>
-              SYNAPSE
-              PROJECTS UI<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </>}
-          {selectedItem == NavItem.FAVORITES && <>
-            <div>FAVORITES UI<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
-          </>}
+          <div>
+            SYNAPSE
+            PROJECTS UI<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </>}
+        {selectedItem == NavItem.FAVORITES && <>
+          <div>FAVORITES UI<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+        </>}
       </Drawer>
     </div>
   )
