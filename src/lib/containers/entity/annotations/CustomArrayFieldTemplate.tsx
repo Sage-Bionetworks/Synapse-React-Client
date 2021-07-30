08b169ce6f524bf4f845a5d3180af27b5cc734f3
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-07-30 13:01:12 -0400

    SWC-5703

--- src/lib/containers/entity/annotations/CustomArrayFieldTemplate.tsx
+++ src/lib/containers/entity/annotations/CustomArrayFieldTemplate.tsx
@@ -29,17 +29,14 @@ export function CustomArrayFieldTemplate<T>(props: ArrayFieldTemplateProps<T>) {
           {props.items.map((element, index) => (
             <div key={element.key} className="array-item">
               {element.children}
-              {(props.schema[rsjfUtils.ADDITIONAL_PROPERTY_FLAG] ||
-                !(index === 0 && props.items.length === 1)) && (
-                <Button
-                  aria-label={`Remove ${props.title}-${index}`}
-                  variant="transparent-primary-500"
-                  className="RemoveButton"
-                  onClick={element.onDropIndexClick(element.index)}
-                >
-                  <Close />
-                </Button>
-              )}
+              <Button
+                aria-label={`Remove ${props.title}-${index}`}
+                variant="transparent-primary-500"
+                className="RemoveButton"
+                onClick={element.onDropIndexClick(element.index)}
+              >
+                <Close />
+              </Button>
               {props.canAdd && index === props.items.length - 1 && (
                 <Button
                   aria-label={`Add new ${props.title}`}
@@ -57,11 +54,11 @@ export function CustomArrayFieldTemplate<T>(props: ArrayFieldTemplateProps<T>) {
             <div style={{ display: 'flex' }}>
               <Button
                 variant="primary-500"
-                className="AddButton"
+                // className="AddButton"
                 onClick={props.onAddClick}
                 type="button"
               >
-                <Add />
+                Add Item <Add />
               </Button>
             </div>
           )}
