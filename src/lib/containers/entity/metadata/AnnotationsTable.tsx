4f9e2339eb67619331d6f878b99cf50a677ac151
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-08-17 10:00:52 -0400

    Rearrange elements

--- src/lib/containers/entity/metadata/AnnotationsTable.tsx
+++ src/lib/containers/entity/metadata/AnnotationsTable.tsx
@@ -31,7 +31,9 @@ export const AnnotationsTable: React.FC<AnnotationsTableProps> = ({
     enabled: isInExperimentalMode,
   })
 
-  return (
+  return isLoading ? (
+    <SkeletonTable numRows={3} numCols={2} />
+  ) : (
     <>
       {entityMetadata && annotations && isEmpty(annotations) ? (
         <div className="placeholder">
@@ -42,51 +44,46 @@ export const AnnotationsTable: React.FC<AnnotationsTableProps> = ({
           has no annotations.
         </div>
       ) : null}
-
-      {isLoading ? (
-        <SkeletonTable numRows={3} numCols={2} />
-      ) : (
-        <table className="AnnotationsTable">
-          <tbody>
-            {annotations &&
-              Object.keys(annotations).map((key: string) => {
-                return (
-                  <tr key={key} className="AnnotationsTable__Row">
-                    <td className="AnnotationsTable__Row__Key">{key}</td>
-                    <td className="AnnotationsTable__Row__Value">
-                      {Array.isArray(annotations[key])
-                        ? (annotations[key] as
-                            | string[]
-                            | number[]
-                            | boolean[]).join(', ')
-                        : annotations[key]!.toString()}
-                    </td>
-                  </tr>
-                )
-              })}
-            {boundSchema && isInExperimentalMode ? (
-              <tr className="AnnotationsTable__Row">
-                <td className="AnnotationsTable__Row__Key Schema">
-                  Validation Schema
-                </td>
-                <td className="AnnotationsTable__Row__Value">
-                  <a
-                    href={`${getEndpoint(
-                      BackendDestinationEnum.REPO_ENDPOINT,
-                    )}repo/v1/schema/type/registered/${
-                      boundSchema.jsonSchemaVersionInfo.$id
-                    }`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {boundSchema.jsonSchemaVersionInfo.schemaName}
-                  </a>
-                </td>
-              </tr>
-            ) : null}
-          </tbody>
-        </table>
-      )}
+      <table className="AnnotationsTable">
+        <tbody>
+          {annotations &&
+            Object.keys(annotations).map((key: string) => {
+              return (
+                <tr key={key} className="AnnotationsTable__Row">
+                  <td className="AnnotationsTable__Row__Key">{key}</td>
+                  <td className="AnnotationsTable__Row__Value">
+                    {Array.isArray(annotations[key])
+                      ? (annotations[key] as
+                          | string[]
+                          | number[]
+                          | boolean[]).join(', ')
+                      : annotations[key]!.toString()}
+                  </td>
+                </tr>
+              )
+            })}
+          {boundSchema && isInExperimentalMode ? (
+            <tr className="AnnotationsTable__Row">
+              <td className="AnnotationsTable__Row__Key Schema">
+                Validation Schema
+              </td>
+              <td className="AnnotationsTable__Row__Value">
+                <a
+                  href={`${getEndpoint(
+                    BackendDestinationEnum.REPO_ENDPOINT,
+                  )}repo/v1/schema/type/registered/${
+                    boundSchema.jsonSchemaVersionInfo.$id
+                  }`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {boundSchema.jsonSchemaVersionInfo.schemaName}
+                </a>
+              </td>
+            </tr>
+          ) : null}
+        </tbody>
+      </table>
     </>
   )
 }
