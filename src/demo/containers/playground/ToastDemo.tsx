73e4d9db8b792d472a726c0584c070ddfbc7b7a4
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-08-20 11:39:36 -0400

    Fix close/primary button overlap on wide screens

--- src/demo/containers/playground/ToastDemo.tsx
+++ src/demo/containers/playground/ToastDemo.tsx
@@ -14,9 +14,20 @@ export const ToastDemo: React.FunctionComponent = () => {
   const [title, setTitle] = useState<string>('')
   const [description, setDescription] = useState<string>('')
   const [autoClose, setAutoClose] = useState<number>(0)
+  const [buttonText, setButtonText] = useState<string>('')
+  const [linkText, setLinkText] = useState<string>('')
 
   function fireToast() {
-    displayToast(variant, title, description, autoClose)
+    displayToast(
+      variant,
+      title,
+      description,
+      autoClose,
+      buttonText ? buttonText : undefined,
+      () => {},
+      linkText ? linkText : undefined,
+      '#',
+    )
   }
   return (
     <div className="bootstrap-4-backport">
@@ -48,6 +59,17 @@ export const ToastDemo: React.FunctionComponent = () => {
         value={autoClose}
         onChange={e => setAutoClose(Number.parseInt(e.target.value))}
       />
+      <FormLabel>Optional Button Text</FormLabel>
+      <FormControl
+        value={buttonText}
+        onChange={e => setButtonText(e.target.value)}
+      />
+      <FormLabel>Optional Link Text</FormLabel>
+      <FormControl
+        value={linkText}
+        onChange={e => setLinkText(e.target.value)}
+      />
+
       <Button variant="primary-500" onClick={fireToast}>
         Push toast message
       </Button>
