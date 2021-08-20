21d7893641205c94944e959f4f66aaa3e74ed1fb
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-08-20 10:32:38 -0400

    Add demo for creating toasts

---
+++ src/demo/containers/playground/ToastDemo.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Button, FormControl, FormLabel } from 'react-bootstrap'
+import { displayToast } from '../../../lib/containers/ToastMessage'
+import { RadioGroup } from '../../../lib/containers/widgets/RadioGroup'
+
+/**
+ * A simple component that displays all of our reusable button styles
+ */
+export const ToastDemo: React.FunctionComponent = () => {
+  const [variant, setVariant] = useState<
+    'info' | 'success' | 'warning' | 'danger'
+  >('info')
+
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [autoClose, setAutoClose] = useState<number>(0)
+
+  function fireToast() {
+    displayToast(variant, title, description, autoClose)
+  }
+  return (
+    <div className="bootstrap-4-backport">
+      <h2>Toast Messages</h2>
+      <FormLabel>Alert Variant</FormLabel>
+      <RadioGroup
+        id="toast-demo-variant"
+        options={[
+          { label: 'Info', value: 'info' },
+          { label: 'Success', value: 'success' },
+          { label: 'Warning', value: 'warning' },
+          { label: 'Danger', value: 'danger' },
+        ]}
+        value={variant}
+        onChange={value =>
+          setVariant(value as 'info' | 'success' | 'warning' | 'danger')
+        }
+      />
+      <FormLabel>Title</FormLabel>
+      <FormControl value={title} onChange={e => setTitle(e.target.value)} />
+      <FormLabel>Description</FormLabel>
+      <FormControl
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <FormLabel>Auto-close (ms)</FormLabel>
+      <FormControl
+        type="number"
+        value={autoClose}
+        onChange={e => setAutoClose(Number.parseInt(e.target.value))}
+      />
+      <Button variant="primary-500" onClick={fireToast}>
+        Push toast message
+      </Button>
+    </div>
+  )
+}
