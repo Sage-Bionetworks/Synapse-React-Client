5ae715f335e2db6d84888a9b4a6cc1feafd012e8
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-09-02 15:20:38 -0400

    SWC-5762

--- src/lib/containers/entity/annotations/SchemaDrivenAnnotationEditor.tsx
+++ src/lib/containers/entity/annotations/SchemaDrivenAnnotationEditor.tsx
@@ -49,6 +49,30 @@ export type SchemaDrivenAnnotationEditorModalProps = {
   onHide: () => void
 }
 
+/**
+ * Strips null values from arrays in the provided form data. If the array is empty after
+ * removing null values, the key is removed from the form data.
+ *
+ * This allows users to submit forms with empty array fields (SWC-5762)
+ */
+function dropNullishArrayValues(
+  formData: Record<string, unknown>,
+): Record<string, unknown> {
+  const newFormData: Record<string, unknown> = {}
+  Object.keys(formData).forEach(key => {
+    let value = formData[key]
+    if (Array.isArray(value)) {
+      value = (value as Array<any>).filter((item: any) => item != null)
+      if (!isEmpty(value)) {
+        newFormData[key] = value
+      }
+    } else {
+      newFormData[key] = value
+    }
+  })
+  return newFormData
+}
+
 function getFriendlyPropertyName(error: AjvError) {
   if (error.property.startsWith('[')) {
     // Additional properties are surrounded by brackets and quotations, so let's remove them
@@ -155,7 +179,7 @@ export const SchemaDrivenAnnotationEditor: React.FunctionComponent<SchemaDrivenA
 
   const mutation = useUpdateViaJson(
     entityId!,
-    { ...formData, ...entityJson },
+    { ...dropNullishArrayValues(formData ?? {}), ...entityJson },
     {
       onSuccess: () => {
         onSuccess()
@@ -183,8 +207,8 @@ export const SchemaDrivenAnnotationEditor: React.FunctionComponent<SchemaDrivenA
               variant="info"
               transition={false}
             >
-              <b>{entityJson.name as string}</b> requires scientific annotations
-              specified by <b>{schema.jsonSchemaVersionInfo.$id}</b>
+              <b>{entityJson.name}</b> requires scientific annotations specified
+              by <b>{schema.jsonSchemaVersionInfo.$id}</b>
               {'. '}
               <b>
                 <a
