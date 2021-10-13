59fda05bce233abb2643adc969634fba74d6dc6e
Jay Hodgson <jay.hodgson@sagebase.org>
2021-10-13 09:29:21 -0700

    PORTALS-1512: remove index, use tooltip text as the key

--- src/lib/containers/widgets/facet-nav/SelectionCriteriaPill.tsx
+++ src/lib/containers/widgets/facet-nav/SelectionCriteriaPill.tsx
@@ -19,7 +19,6 @@ export type FacetWithSelection = {
 
 export type SelectionCriteriaPillProps = {
   facetWithSelection?: FacetWithSelection
-  index: number
   className?: string
   onRemove: Function
   filter?: {
@@ -30,7 +29,6 @@ export type SelectionCriteriaPillProps = {
 
 const SelectionCriteriaPill: FunctionComponent<SelectionCriteriaPillProps> = ({
   facetWithSelection,
-  index,
   onRemove,
   filter,
 }) => {
@@ -43,10 +41,8 @@ const SelectionCriteriaPill: FunctionComponent<SelectionCriteriaPillProps> = ({
           ? SynapseConstants.FRIENDLY_VALUE_NOT_SET
           : facetWithSelection.displayValue
     } else {
-      innerText =
-        (facetWithSelection.facet as FacetColumnResultRange).selectedMin +
-        ' - ' +
-        (facetWithSelection.facet as FacetColumnResultRange).selectedMax
+      innerText = `${(facetWithSelection.facet as FacetColumnResultRange).selectedMin} 
+        - ${(facetWithSelection.facet as FacetColumnResultRange).selectedMax}`
     }
     tooltipText = `${unCamelCase(
       facetWithSelection.facet.columnName,
@@ -62,13 +58,13 @@ const SelectionCriteriaPill: FunctionComponent<SelectionCriteriaPillProps> = ({
   }
   return (
     <ElementWithTooltip
-      idForToolTip={`selectionCriteria_${+index}`}
+      idForToolTip={`selectionCriteria_${tooltipText}`}
       tooltipText={tooltipText}
       callbackFn={() => {}}
     >
       <label
         className="SelectionCriteriaPill"
-        key={index + '__SelectionCriteriaPill'}
+        key={`SelectionCriteriaPill ${tooltipText}`}
       >
         <span>{innerText}</span>
         <button
