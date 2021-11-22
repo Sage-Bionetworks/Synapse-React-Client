0df2087d562d35d6375023d10001843d74ca8cc8
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-11-22 13:50:15 -0500

    add useResizeHandler as a prop

--- src/lib/containers/PlotlyWrapper.tsx
+++ src/lib/containers/PlotlyWrapper.tsx
@@ -9,35 +9,35 @@ export type PlotlyWrapperProps = {
   data: Plotly.Data[]
   layout?: Partial<Plotly.Layout>
   config?: Partial<Plotly.Config>
+  useResizeHandler?: boolean
   className?: string
   containerWidth?: number
 }
 
 const PlotlyWrapper: React.FC<PlotlyWrapperProps> = props => {
-  const { data, layout, config, className, containerWidth } = props
+  const { data, layout, config, className, containerWidth, useResizeHandler } =
+    props
   const hasData = !!(data && data.length)
 
-  return (<div className={className}>
-    {
-      !hasData &&
-      <>
-        <div
-          className={"chart-nodata"}
-          style={{width: containerWidth}}>
-          <span>Data Unavailable</span>
-        </div>
-      </>
-    }
-    {
-      hasData &&
-      <Plot
-        data={data}
-        layout={layout ?? {}}
-        config={config}
-      />
-    }
-
-  </div>)
+  return (
+    <div className={className}>
+      {!hasData && (
+        <>
+          <div className={'chart-nodata'} style={{ width: containerWidth }}>
+            <span>Data Unavailable</span>
+          </div>
+        </>
+      )}
+      {hasData && (
+        <Plot
+          data={data}
+          layout={layout ?? {}}
+          config={config}
+          useResizeHandler={useResizeHandler}
+        />
+      )}
+    </div>
+  )
 }
 
 export default PlotlyWrapper
