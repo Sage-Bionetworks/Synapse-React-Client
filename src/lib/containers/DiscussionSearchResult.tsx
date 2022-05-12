c03e8724cbd1c9cf9a16a069c30021a79e3f3638
Yongjoo Yoo <yjoo.eyoo@gmail.com>
2022-05-12 10:08:20 -0700

    SWC-6101: use IconSvg, use Skeleton for loader, and error handling

--- src/lib/containers/DiscussionSearchResult.tsx
+++ src/lib/containers/DiscussionSearchResult.tsx
@@ -4,12 +4,13 @@ import { useSynapseContext } from '../utils/SynapseContext'
 import { UserProfile } from '../utils/synapseTypes'
 import { DiscussionReplyBundle, DiscussionThreadBundle } from '../utils/synapseTypes/DiscussionBundle'
 import moment from 'moment'
-import ReplyIcon from '../assets/icons/reply.svg'
-import ThreadIcon from '../assets/icons/thread.svg'
 import Typography from '../utils/typography/Typography'
 import { Col, Row } from 'react-bootstrap'
 import UserCard from './UserCard'
 import { SMALL_USER_CARD } from '../utils/SynapseConstants'
+import IconSvg from './IconSvg'
+import { Skeleton } from '@material-ui/lab'
+import { SkeletonTable } from '../assets/skeletons/SkeletonTable'
 
 export const getMessage = async (url: string): Promise<any> => {
     const response = await fetch(url, {
@@ -35,51 +36,69 @@ const DiscussionSearchResult = (props: DiscussionSearchResultProps) => {
     const [messageUrl, setMessageUrl] = useState<string>('')
     const [replyBundle, setReplyBundle] = useState<DiscussionReplyBundle>()
     const [replyAuthor, setReplyAuthor] = useState<UserProfile>()
+    const [isLoading, setIsLoading] = useState(false)
 
     const getThreadOrReply = async () => {
-        let someUrl
+        let newMessageUrl
         const thread = await getThread(threadId, accessToken)
+        setIsLoading(true)
         if (replyId) {
             const reply = await getReply(replyId, accessToken)
-            someUrl = await getReplyMessageUrl(reply.messageKey, accessToken)
+            newMessageUrl = await getReplyMessageUrl(reply.messageKey, accessToken)
             setReplyAuthor(await getUserProfileById(accessToken, reply.createdBy))
             setReplyBundle(reply)
         } else {
             setReplyAuthor(await getUserProfileById(accessToken, thread.createdBy))
-            someUrl = await getThreadMessageUrl(thread.messageKey, accessToken)
+            newMessageUrl = await getThreadMessageUrl(thread.messageKey, accessToken)
         }
-        setMessageUrl(await getMessage(someUrl.messageUrl))
+        setMessageUrl(await getMessage(newMessageUrl.messageUrl))
         setThreadBundle(thread)
+        setIsLoading(false)
     }
 
     useEffect(() => {
         getThreadOrReply()
     }, [])
 
-    const getUrl = (threadId: string, replyId: string, projectId: string) => {
+    const getUrl = (threadId: string, projectId: string, replyId?: string,) => {
         return `https://synapse.org/#!Synapse:${projectId}/discussion/threadId=${threadId}&replyId=${replyId}`
     }
 
     return (
         <div className='search-result-container'>
-            <Row>
-                <Col xs={1}>
-                    <img src={replyId ? ReplyIcon : ThreadIcon} alt={replyId ? 'reply-icon' : 'thread-icon'} className='search-result-icon' />
-                </Col>
-                <Col xs={11}>
-                    <Typography variant='headline3'>
-                        <a className='link' href={getUrl(threadBundle?.id!, replyBundle?.id!, threadBundle?.projectId!)}>{threadBundle?.title}</a>
-                    </Typography>
-                    <div className='truncated'>
-                        <Typography variant='body1'>
-                            {messageUrl}
+            {isLoading ?
+                <LoadingDiscussionSearchResult />
+                :
+                <Row>
+                    <Col xs={1}>
+                        {replyId ? <IconSvg options={{ icon: 'replyTwoTone' }} /> : <IconSvg options={{ icon: 'chatTwoTone' }} />}
+                    </Col>
+                    <Col xs={11}>
+                        <Typography variant='headline3'>
+                            <a className='link' href={getUrl(threadBundle?.id!, replyBundle?.id!, threadBundle?.projectId!)}>{threadBundle?.title}</a>
                         </Typography>
-                    </div>
-                    <div className='search-result-footer'>
-                        {replyId ? 'Reply' : 'Thread'} by {<UserCard size={SMALL_USER_CARD} ownerId={replyAuthor?.ownerId!} />} {moment(replyBundle?.createdOn).format('l LT')}
-                    </div>
-                </Col>
-            </Row>
+                        <div className='truncated'>
+                            <Typography variant='body1'>
+                                {messageUrl}
+                            </Typography>
+                        </div>
+                        <div className='search-result-footer'>
+                            {replyId ? 'Reply' : 'Thread'} by {<UserCard size={SMALL_USER_CARD} ownerId={replyAuthor?.ownerId!} />} {moment(replyBundle?.createdOn).format('l LT')}
+                        </div>
+                    </Col>
+                </Row>
+            }
+        </div>
+    )
+}
+
+export const LoadingDiscussionSearchResult: React.FunctionComponent = () => {
+    return (
+        <div>
+            <Skeleton />
+            <div>
+                <SkeletonTable numCols={1} numRows={3} />
+            </div>
         </div>
     )
 }
--- src/lib/containers/ForumSearch.tsx
+++ src/lib/containers/ForumSearch.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { forumSearch, getEntity } from '../utils/SynapseClient'
 import { DiscussionSearchResponse, Match } from '../utils/synapseTypes/DiscussionSearch'
 import { useSynapseContext } from '../utils/SynapseContext'
 import DiscussionReply from './DiscussionSearchResult'
-import { SynapseSpinner } from './LoadingScreen'
 import { Entity } from '../utils/synapseTypes'
 import Typography from '../utils/typography/Typography'
 import NoSearchResults from '../assets/icons/NoSearchResults'
 import { Button } from 'react-bootstrap'
+import IconSvg from './IconSvg'
+import { displayToast } from './ToastMessage'
 
 export type ForumSearchProps = {
     forumId: string,
@@ -18,7 +17,6 @@ export type ForumSearchProps = {
 
 const ForumSearch = (props: ForumSearchProps) => {
     const { accessToken } = useSynapseContext()
-    const [isLoading, setIsLoading] = useState(false)
     const [searchInput, setSearchInput] = useState<string>('')
     const [searchResult, setSearchResult] = useState<DiscussionSearchResponse>()
     const [matchList, setMatchList] = useState<Match[]>()
@@ -27,21 +25,23 @@ const ForumSearch = (props: ForumSearchProps) => {
 
     const onSearch = async () => {
         setSearchResult(undefined)
-        setIsLoading(true)
         setNoSearchResult(false)
-        const searchResponse = await forumSearch(
-            {
-                searchString: searchInput,
-                nextPageToken: searchResult?.nextPageToken ?? undefined
-            },
-            props.forumId,
-            accessToken)
-        if (searchResponse.matches.length == 0) {
-            setNoSearchResult(true)
+        try{
+            const searchResponse = await forumSearch(
+                {
+                    searchString: searchInput,
+                    nextPageToken: searchResult?.nextPageToken ?? undefined
+                },
+                props.forumId,
+                accessToken)
+            if (searchResponse.matches.length == 0) {
+                setNoSearchResult(true)
+            }
+            setSearchResult(searchResponse)
+            setMatchList(searchResponse.matches)
+        } catch(err: any){
+            displayToast(err.reason as string, 'danger')
         }
-        setSearchResult(searchResponse)
-        setMatchList(searchResponse.matches)
-        setIsLoading(false)
     }
 
     useEffect(() => {
@@ -81,11 +81,7 @@ const ForumSearch = (props: ForumSearchProps) => {
     return (
         <div className="bootstrap-4-backport ForumSearch">
             <div>
-                <FontAwesomeIcon
-                    size={'sm'}
-                    icon={faSearch}
-                    className="SearchIcon"
-                />
+                <IconSvg options={{ icon: 'searchTwoTone' }} />
                 <input
                     role='textbox'
                     type='search'
@@ -102,42 +98,35 @@ const ForumSearch = (props: ForumSearchProps) => {
                     }}
                 />
                 {searchInput && (
-                    <FontAwesomeIcon
-                        size={'sm'}
-                        icon={faTimes}
-                        role="button"
-                        title="Clear Search"
-                        className="ClearSearchIcon"
+                    <button
+                        className='ClearSearchIcon'
                         onClick={() => {
                             setSearchInput('')
-                        }}
-                    />
+                        }}>
+                        <IconSvg options={{ icon: 'clear' }} />
+                    </button>
                 )}
             </div>
             {
                 noSearchResult && <NoSearchResultComponent />
             }
             {
-                isLoading ?
-                    <div style={{ textAlign: 'center' }}>
-                        <SynapseSpinner size={40} />
-                    </div> :
-                    matchList &&
-                    <>
-                        {props.projectId && (
-                            <Typography variant='body2'>Results for '{searchInput}' in {entity?.name}</Typography>
-                        )}
-                        {matchList.map(entity =>
-                            <div key={`${entity.forumId}-${entity.threadId}-${entity.replyId}`}>
-                                <DiscussionReply threadId={entity.threadId} replyId={entity.replyId} />
-                            </div>
-                        )}
-                    </>
+                matchList &&
+                <>
+                    {props.projectId && (
+                        <Typography variant='body2'>Results for '{searchInput}' in {entity?.name}</Typography>
+                    )}
+                    {matchList.map(match =>
+                        <div key={`${match.forumId}-${match.threadId}-${match.replyId}`}>
+                            <DiscussionReply threadId={match.threadId} replyId={match.replyId} />
+                        </div>
+                    )}
+                </>
             }
             {
                 searchResult?.nextPageToken &&
                 <div className='text-center'>
-                    <Button onClick={onLoadMore}>Load more</Button>
+                    <Button variant='primary' onClick={onLoadMore}>Load more</Button>
                 </div>
             }
         </div>
