29a1adf97333be04498d617e01662b2dcf7fab17
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-06-07 12:04:21 -0400

    useDebouncedEffect

---
+++ src/lib/utils/hooks/useDebouncedEffect.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+
+/**
+ * Debounces execution of an effect based on a specified delay.
+ * See https://stackoverflow.com/a/61127960
+ * @param effect
+ * @param deps
+ * @param delay
+ */
+export const useDebouncedEffect = (
+  effect: React.EffectCallback,
+  deps: React.DependencyList,
+  delay: number,
+) => {
+  useEffect(() => {
+    const handler = setTimeout(() => effect(), delay)
+
+    return () => clearTimeout(handler)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, react-hooks/exhaustive-deps
+  }, [...(deps || []), delay])
+}
