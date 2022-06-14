e619919a66bc7e1cf0fb31923e7d0533c327cd76
Yongjoo Yoo <yjoo.eyoo@gmail.com>
2022-06-14 10:35:13 -0700

    SWC-6121

---
+++ src/lib/containers/dataaccess/AccessHistoryDashboard.tsx
@@ -0,0 +1,160 @@
+import { omitBy } from 'lodash-es'
+import React, { useCallback, useEffect, useState } from 'react'
+import { FormLabel } from 'react-bootstrap'
+import { useHistory, useLocation } from 'react-router-dom'
+import { useDebouncedEffect } from '../../utils/hooks/useDebouncedEffect'
+import Typography from '../../utils/typography/Typography'
+import {
+  AccessApprovalsTable,
+  AccessApprovalsTableProps,
+} from '../AccessApprovalsTable'
+import {
+  AccessRequestSubmissionTable,
+  AccessRequestSubmissionTableProps,
+} from '../AccessRequestSubmissionTable'
+import UserOrTeamBadge from '../UserOrTeamBadge'
+import UserSearchBoxV2 from '../UserSearchBoxV2'
+import AccessRequirementSearchBox from './AccessRequirementSearchBox'
+import NoSearchResult from '../../assets/icons/NoSearchResultSmile'
+
+export type UserHistoryDashboardProps = {}
+
+export const UserHistoryDashboard: React.FunctionComponent<
+  UserHistoryDashboardProps
+> = () => {
+  const [accessRequirementId, setAccessRequirementId] = useState<
+    string | undefined
+  >()
+  const [accessorId, setAccessorId] = useState<string>('')
+  const [approvalTableProps, setApprovalTableProps] =
+    useState<AccessApprovalsTableProps>({
+      accessorId,
+      accessRequirementId,
+    })
+  const [submissionTableProps, setSubmissionTableProps] =
+    useState<AccessRequestSubmissionTableProps>({
+      showStatus: true,
+      showSubmitter: true,
+      accessorId,
+      accessRequirementId,
+    })
+
+  const location = useLocation()
+  const history = useHistory()
+  const INPUT_CHANGE_DEBOUNCE_DELAY_MS = 500
+
+  const onAccessorChange = useCallback((selected: string | null) => {
+    if (selected) {
+      setAccessorId(selected)
+    } else {
+      setAccessorId('')
+    }
+  }, [])
+
+  useEffect(() => {
+    const initializeFromSearchParam = () => {
+      const initialParams = new URLSearchParams(location.search)
+      setAccessRequirementId(
+        initialParams.get('accessRequirementId') ?? undefined,
+      )
+      setAccessorId(initialParams.get('accessorId') ?? '')
+    }
+    initializeFromSearchParam()
+  }, [location.search])
+
+  useDebouncedEffect(
+    () => {
+      const updateQueryParams = (
+        accessRequirementId: string | undefined,
+        accessorId: string,
+      ) => {
+        const params = new URLSearchParams(
+          omitBy(
+            {
+              accessRequirementId,
+              accessorId,
+            },
+            item => item === undefined || item === '',
+          ) as Record<string, string>,
+        )
+        history.replace({
+          pathname: location.pathname,
+          search: params.toString(),
+        })
+      }
+
+      updateQueryParams(accessRequirementId, accessorId)
+      setApprovalTableProps({
+        accessorId,
+        accessRequirementId,
+      })
+      setSubmissionTableProps({
+        showStatus: true,
+        showSubmitter: true,
+        accessorId,
+        accessRequirementId,
+      })
+    },
+    [accessRequirementId, accessorId, history, location],
+    INPUT_CHANGE_DEBOUNCE_DELAY_MS,
+  )
+
+  const NoSearchResultComponent = () => {
+    return (
+      <div className="text-center">
+        {NoSearchResult}
+        <Typography variant="body2">
+          Enter a user or team name in the search field above to view their data
+          access history
+        </Typography>
+      </div>
+    )
+  }
+
+  return (
+    <div className="UserHistoryDashboard bootstrap-4-backport">
+      <div className="InputPanel SubmissionGrid">
+        <div>
+          <FormLabel htmlFor="user-team-filter">
+            Select a user or team name to view their access history
+          </FormLabel>
+          <UserSearchBoxV2
+            htmlId="user-team-filter"
+            onChange={onAccessorChange}
+            defaultValue={accessorId}
+            placeholder="Search for a user or team name"
+          />
+        </div>
+
+        {accessorId && (
+          <div>
+            <FormLabel htmlFor="arName-filter">
+              Filter by Access Requirement Name
+            </FormLabel>
+            <AccessRequirementSearchBox
+              htmlId="arName-filter"
+              placeholder="Search for an Access Requirement Name "
+              onChange={setAccessRequirementId}
+            />
+          </div>
+        )}
+      </div>
+
+      {accessorId ? (
+        <>
+          <Typography variant="headline3">
+            Submissions including <UserOrTeamBadge principalId={accessorId} />
+          </Typography>
+          <AccessRequestSubmissionTable {...submissionTableProps} />
+
+          <Typography variant="headline3">
+            Status in Access Requirements
+          </Typography>
+          <AccessApprovalsTable {...approvalTableProps} />
+        </>
+      ) : (
+        <NoSearchResultComponent />
+      )}
+    </div>
+  )
+}
