802b1fb93ad583213dc5f6e8ac1ac165717e6f4a
nickgros <nickgrosenbacher@gmail.com>
2022-07-14 12:52:51 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/Synapse-React-Client@ad3681dfc852a3cc29ba6dc3536ed3ae075396ad 🚀

---
+++ assets/useGetDownloadListStatistics.ddfc878e.js
@@ -0,0 +1,2 @@
+import{H as o,t as a,bU as e}from"./index.1cc5f672.js";function i(s){const{accessToken:t}=a();return o(["downloadliststatsv2",t],()=>e(t),s)}export{i as u};
+//# sourceMappingURL=useGetDownloadListStatistics.ddfc878e.js.map
