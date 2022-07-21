809b7cc1983d91a5e9b97b8a00f7186d3555faff
nickgros <nickgrosenbacher@gmail.com>
2022-07-21 20:03:42 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/Synapse-React-Client@7c0ed88459721b168aecfce2200a6141bda5c098 🚀

--- assets/isEqual.7fcc583a.js
+++
@@ -1,2 +0,0 @@
-import{b as s}from"./_baseIsEqual.6576b742.js";function o(a,r){return s(a,r)}export{o as i};
-//# sourceMappingURL=isEqual.7fcc583a.js.map
--- assets/useListState.7f5a3e64.js
+++
@@ -1,2 +0,0 @@
-const d=window.React.useState,l=o=>{const[s,i]=d(o);return{list:s,handleListChange:e=>t=>{const n=[...s];n[e]=t,i(n)},handleListRemove:e=>()=>{const t=s.filter((n,a)=>e!==a);i(t)},appendToList:(...e)=>{const t=[...s];t.push(...e),i(t)},setList:i}};export{l as u};
-//# sourceMappingURL=useListState.7f5a3e64.js.map
