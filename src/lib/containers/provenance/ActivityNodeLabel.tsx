050390322c5cc03b4dc4d70bbfa49fcfbceda9a4
Jay Hodgson <jay.hodgson@sagebase.org>
2022-07-22 16:13:53 -0700

    improve the entity and activity node

--- src/lib/containers/provenance/ActivityNodeLabel.tsx
+++ src/lib/containers/provenance/ActivityNodeLabel.tsx
@@ -1,15 +1,20 @@
+import moment from 'moment'
 import React from 'react'
 import { Handle, Position } from 'react-flow-renderer'
+import { formatDate } from '../../utils/functions/DateFormatter'
+import { useGetUserProfile } from '../../utils/hooks/SynapseAPI'
 import { Activity } from '../../utils/synapseTypes/Provenance/Provenance'
-import Typography from '../../utils/typography/Typography'
+import { UserCardSmall } from '../UserCardSmall'
 
 export type ActivityNodeLabelProps = Activity
 export const ActivityNodeLabel = (data: ActivityNodeLabelProps) => {
+  const { data: userProfile } = useGetUserProfile(data.modifiedBy)
+  const friendlyModifiedOn = formatDate(moment(data.modifiedOn))
   return (
     <>
       <Handle type="target" position={Position.Top} isConnectable={false} />
-      <Typography variant="headline3">Activity</Typography>
-      <div>{data?.name}</div>
+      {userProfile && <UserCardSmall userProfile={userProfile} />}
+      <div>{friendlyModifiedOn}</div>
       <Handle type="source" position={Position.Bottom} isConnectable={false} />
     </>
   )
--- src/lib/containers/provenance/EntityNodeLabel.tsx
+++ src/lib/containers/provenance/EntityNodeLabel.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { Handle, Position } from 'react-flow-renderer'
+import { convertToEntityType } from '../../utils/functions/EntityTypeUtils'
+import { PRODUCTION_ENDPOINT_CONFIG } from '../../utils/functions/getEndpoint'
 import { useGetEntity } from '../../utils/hooks/SynapseAPI'
-import { Reference } from '../../utils/synapseTypes'
-import Typography from '../../utils/typography/Typography'
+import { EntityType, Reference } from '../../utils/synapseTypes'
+import { EntityTypeIcon } from '../EntityIcon'
 
 export type EntityNodeLabelProps = Reference
 
 export const EntityNodeLabel = (data: EntityNodeLabelProps) => {
   const { targetId, targetVersionNumber } = data
   const { data: entity } = useGetEntity(targetId, targetVersionNumber)
+  const entityType: EntityType = entity?.concreteType
+    ? convertToEntityType(entity?.concreteType)
+    : EntityType.FILE
+  const entityVersionString =
+    entity && 'versionNumber' in entity ? `.${entity['versionNumber']}` : ''
   return (
     <>
       <Handle type="target" position={Position.Top} isConnectable={false} />
-      <Typography variant="headline3">Entity</Typography>
-      <div>{entity?.name}</div>
+      <EntityTypeIcon type={entityType} includeTooltip={false} />
+      <a
+        href={`${PRODUCTION_ENDPOINT_CONFIG.PORTAL}#!Synapse:${entity?.id}${entityVersionString}`}
+      >
+        {entity?.name}
+      </a>
       <Handle type="source" position={Position.Bottom} isConnectable={false} />
     </>
   )
