7c73613f0700537b42590dc13bac311528d13ea3
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-10-05 16:32:49 -0400

    Fix compile error

--- src/lib/utils/SynapseConstants.ts
+++ src/lib/utils/SynapseConstants.ts
@@ -10,7 +10,7 @@ export const ANONYMOUS_PRINCIPAL_ID = 273950
 /** The Synapse Access and Compliance team */
 export const ACT_TEAM_ID = 464532
 /** The team containing Synapse users whose HTML files may be rendered without sanitization */
-export const TRUSTED_HTML_USERS_TEAM_ID = 3351236
+export const TRUSTED_HTML_USERS_TEAM_ID = '3351236'
 
 /** QueryBundleRequest constants */
 export const BUNDLE_MASK_QUERY_RESULTS: number = 1
