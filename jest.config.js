c5533151f9549109f40ca5923d565022dabbeeab
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-10-10 14:00:12 -0400

    PORTALS-2339
    Fix tests, drop ts-jest, upgrade babel

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,19 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const esModules = [
+  '@juggle/resize-observer',
+  '@react-hook',
+  'lodash-es',
+  'nanoid',
+].join('|')
+
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
     // Mock SVGs, loaded by SVGR https://react-svgr.com/docs/jest/
     '\\.svg$': '<rootDir>/src/mocks/svg.js',
   },
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!(nanoid|lodash-es|jest*)/)',
-  ],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   resetMocks: false,
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-      // diagnostics: false,
-      isolatedModules: true,
-    },
-  },
 }
