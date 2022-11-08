3aa7d8a819e9e4a5bca29b579c625d49fa74e6eb
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-11-08 12:50:31 -0500

    SWC-6321
    - Increase cacheTime from 5 min to 30 min

--- src/lib/utils/SynapseContext.tsx
+++ src/lib/utils/SynapseContext.tsx
@@ -12,6 +12,7 @@ export const defaultQueryClientConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 min
+      cacheTime: 1000 * 60 * 30, // 30 min
       retry: false, // SynapseClient knows which queries to retry
     },
   },
