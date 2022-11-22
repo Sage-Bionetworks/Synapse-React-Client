4a0b65bd40419e63182f0b4a54579f325ba6c927
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-11-22 12:48:35 -0500

    SWC-6323
    - Add EntityActionMenu component

---
+++ src/__tests__/lib/containers/entity/page/EntityActionMenu.test.tsx
@@ -0,0 +1,562 @@
+import React from 'react'
+import EntityActionMenu, {
+  ActionConfiguration,
+  ActionConfigurationMap,
+  ActionViewProps,
+  EntityActionMenuLayout,
+  EntityActionMenuProps,
+  EXPORTED_FOR_UNIT_TESTING,
+  MenuConfigurationMap,
+} from '../../../../../lib/containers/entity/page/action_menu/EntityActionMenu'
+import { createWrapper } from '../../../../../lib/testutils/TestingLibraryUtils'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+function renderComponent(props: EntityActionMenuProps) {
+  return render(<EntityActionMenu {...props} />, {
+    wrapper: createWrapper(),
+  })
+}
+
+const onClickFn = jest.fn()
+const ACTION_1 = 'action1'
+const ACTION_2 = 'action2'
+const ACTION_3 = 'action3'
+
+const actionConfiguration1: ActionConfiguration = {
+  text: 'action 1 text',
+  visible: true,
+  onClick: () => {
+    onClickFn(ACTION_1)
+  },
+}
+const actionConfiguration2: ActionConfiguration = {
+  text: 'action 2 text',
+  visible: true,
+  onClick: () => {
+    onClickFn(ACTION_2)
+  },
+}
+const actionConfiguration3: ActionConfiguration = {
+  text: 'action 3 text',
+  visible: true,
+  onClick: () => {
+    onClickFn(ACTION_3)
+  },
+}
+
+const menuConfiguration: MenuConfigurationMap = {
+  DOWNLOAD: {
+    visible: true,
+  },
+  PRIMARY: { visible: true },
+}
+
+const PRIMARY_MENU_TEXT = 'Primary Menu'
+const DOWNLOAD_OPTIONS = 'Download Options'
+
+describe('EntityActionMenu tests', () => {
+  beforeEach(() => jest.clearAllMocks())
+
+  it('Renders icon buttons', async () => {
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [
+        {
+          action: ACTION_1,
+          icon: 'article',
+        },
+      ],
+      downloadMenuActions: [],
+      primaryMenuActions: [],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_1]: actionConfiguration1,
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    const iconButton = await screen.findByRole('button', {
+      name: 'action 1 text',
+    })
+    await userEvent.click(iconButton)
+    expect(onClickFn).toHaveBeenCalledWith(ACTION_1)
+  })
+  it('Renders the primary and dropdown menus', async () => {
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [],
+      downloadMenuActions: [
+        [
+          {
+            action: ACTION_1,
+          },
+        ],
+      ],
+      primaryMenuActions: [[{ action: ACTION_2 }, { action: ACTION_3 }]],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_1]: actionConfiguration1,
+      [ACTION_2]: actionConfiguration2,
+      [ACTION_3]: actionConfiguration3,
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    const downloadMenu = await screen.findByRole('button', {
+      name: DOWNLOAD_OPTIONS,
+    })
+    await userEvent.click(downloadMenu)
+    const action1MenuItem = await screen.findByRole('menuitem', {
+      name: actionConfiguration1.text,
+    })
+    await userEvent.click(action1MenuItem)
+    expect(onClickFn).toHaveBeenCalledWith(ACTION_1)
+
+    const primaryMenu = await screen.findByRole('button', {
+      name: PRIMARY_MENU_TEXT,
+    })
+    await userEvent.click(primaryMenu)
+    const action2MenuItem = await screen.findByRole('menuitem', {
+      name: actionConfiguration2.text,
+    })
+    await userEvent.click(action2MenuItem)
+    expect(onClickFn).toHaveBeenCalledWith(ACTION_2)
+  })
+
+  it('Handles an href dropdown menu item', async () => {
+    const href = 'https://fake-website.gov'
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [],
+      downloadMenuActions: [
+        [
+          {
+            action: ACTION_1,
+          },
+        ],
+      ],
+      primaryMenuActions: [[{ action: ACTION_2 }, { action: ACTION_3 }]],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_1]: actionConfiguration1,
+      [ACTION_2]: { ...actionConfiguration2, onClick: undefined, href: href },
+      [ACTION_3]: actionConfiguration3,
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    const primaryMenu = await screen.findByRole('button', {
+      name: PRIMARY_MENU_TEXT,
+    })
+    await userEvent.click(primaryMenu)
+    const action2MenuItem = await screen.findByRole('menuitem', {
+      name: actionConfiguration2.text,
+    })
+    expect(action2MenuItem).toHaveAttribute('href', href)
+  })
+  it('Renders a button if there is one visible action in the primary dropdown menu', async () => {
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [],
+      downloadMenuActions: [],
+      // Only one action in the primary menu layout
+      primaryMenuActions: [[{ action: ACTION_2 }]],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_2]: actionConfiguration2,
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    // Action button is rendered instead of menu
+    const actionButton = await screen.findByRole('button', {
+      name: actionConfiguration2.text,
+    })
+    await userEvent.click(actionButton)
+    expect(onClickFn).toHaveBeenCalledWith(ACTION_2)
+  })
+
+  it('Does not render the primary menu if it has no visible items', () => {
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [],
+      downloadMenuActions: [],
+      // Only one action in the primary menu layout
+      primaryMenuActions: [],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {}
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    expect(
+      screen.queryByRole('button', { name: PRIMARY_MENU_TEXT }),
+    ).not.toBeInTheDocument()
+  })
+
+  it('Omits items that are not visible', () => {
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [{ action: ACTION_1, icon: 'article' }],
+      downloadMenuActions: [[{ action: ACTION_2 }]],
+      primaryMenuActions: [[{ action: ACTION_3 }]],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_1]: { ...actionConfiguration1, visible: false },
+      [ACTION_2]: { ...actionConfiguration2, visible: false },
+      [ACTION_3]: { ...actionConfiguration3, visible: false },
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    // No primary menu
+    expect(
+      screen.queryByRole('button', { name: PRIMARY_MENU_TEXT }),
+    ).not.toBeInTheDocument()
+
+    // No download menu
+    expect(
+      screen.queryByRole('button', { name: DOWNLOAD_OPTIONS }),
+    ).not.toBeInTheDocument()
+
+    // No icon button
+    expect(
+      screen.queryByRole('button', { name: actionConfiguration1.text }),
+    ).not.toBeInTheDocument()
+  })
+
+  it('Omits groups that contain no visible items', async () => {
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [],
+      downloadMenuActions: [],
+      // Each item in its own group
+      primaryMenuActions: [
+        [{ action: ACTION_1 }],
+        [{ action: ACTION_2 }],
+        [{ action: ACTION_3 }],
+      ],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_1]: actionConfiguration1,
+      // Hide Action 2, which has its own group
+      [ACTION_2]: { ...actionConfiguration2, visible: false },
+      [ACTION_3]: actionConfiguration3,
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    const primaryMenu = await screen.findByRole('button', {
+      name: PRIMARY_MENU_TEXT,
+    })
+    await userEvent.click(primaryMenu)
+    expect(await screen.findAllByRole('menuitem')).toHaveLength(2)
+    // Only one separator should appear, since there are two rendered groups
+    expect(await screen.findAllByRole('separator')).toHaveLength(1)
+  })
+  it('Inserts configured actions into the primary menu if the actions are not specified in the layout', async () => {
+    const consoleWarnSpy = jest
+      .spyOn(console, 'warn')
+      .mockImplementation(() => {})
+
+    // Only one item is specified in the layout
+    const layout: EntityActionMenuLayout = {
+      buttonActions: [],
+      downloadMenuActions: [],
+      primaryMenuActions: [[{ action: ACTION_1 }]],
+      primaryMenuText: PRIMARY_MENU_TEXT,
+      primaryMenuEndIcon: 'verticalEllipsis',
+    }
+
+    // Three items are configured
+    const actionConfiguration: ActionConfigurationMap = {
+      [ACTION_1]: actionConfiguration1,
+      [ACTION_2]: actionConfiguration2,
+      [ACTION_3]: actionConfiguration3,
+    }
+
+    renderComponent({
+      actionConfiguration,
+      menuConfiguration,
+      layout,
+    })
+
+    const primaryMenu = await screen.findByRole('button', {
+      name: PRIMARY_MENU_TEXT,
+    })
+    await userEvent.click(primaryMenu)
+    expect(await screen.findAllByRole('menuitem')).toHaveLength(3)
+    // One separator should appear because the actions not specified in the layout are inserted into the same group
+    expect(await screen.findAllByRole('separator')).toHaveLength(1)
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      'Actions are visible but have not been configured in the layout:',
+      [ACTION_2, ACTION_3],
+    )
+
+    consoleWarnSpy.mockRestore()
+  })
+
+  describe('getUnmappedActions', () => {
+    const { getUnmappedActions } = EXPORTED_FOR_UNIT_TESTING
+    it('Returns actions that are configured but not specified in layout', () => {
+      const ACTION_1 = 'action1'
+      const ACTION_2 = 'action2'
+      const ACTION_3 = 'action3'
+      const ACTION_4 = 'action4'
+      const actionConfigurationMap: ActionConfigurationMap = {
+        [ACTION_1]: {
+          text: 'specified in icon button layout',
+          visible: true,
+        },
+        [ACTION_2]: {
+          text: 'specified in download menu layout',
+          visible: true,
+        },
+        [ACTION_3]: {
+          text: 'specified in primary menu layout',
+          visible: true,
+        },
+        [ACTION_4]: {
+          text: 'not included in layout',
+          visible: true,
+        },
+      }
+      const layout: EntityActionMenuLayout = {
+        buttonActions: [{ action: ACTION_1 }],
+        downloadMenuActions: [[{ action: ACTION_2 }]],
+        primaryMenuActions: [[{ action: ACTION_3 }]],
+        primaryMenuText: 'Menu text',
+        primaryMenuEndIcon: 'verticalEllipsis',
+      }
+
+      // Call under test
+      const result = getUnmappedActions(actionConfigurationMap, layout)
+
+      expect(result).toEqual({ [ACTION_4]: actionConfigurationMap[ACTION_4] })
+    })
+    it('Does not return actions that are not configured', () => {
+      const actionConfigurationMap: ActionConfigurationMap = {
+        // No actions are configured!
+      }
+      const layout: EntityActionMenuLayout = {
+        buttonActions: [],
+        downloadMenuActions: [],
+        primaryMenuActions: [],
+        primaryMenuText: 'Menu text',
+        primaryMenuEndIcon: 'verticalEllipsis',
+      }
+
+      // Call under test
+      const result = getUnmappedActions(actionConfigurationMap, layout)
+
+      expect(result).toEqual({})
+    })
+
+    it('Does not return hidden actions', () => {
+      const actionConfigurationMap: ActionConfigurationMap = {
+        hiddenAction: {
+          text: 'This action is not visible',
+          visible: false,
+        },
+      }
+      const layout: EntityActionMenuLayout = {
+        buttonActions: [],
+        downloadMenuActions: [],
+        primaryMenuActions: [],
+        primaryMenuText: 'Menu text',
+        primaryMenuEndIcon: 'verticalEllipsis',
+      }
+
+      // Call under test
+      const result = getUnmappedActions(actionConfigurationMap, layout)
+
+      expect(result).toEqual({})
+    })
+  })
+
+  describe('mapAndFilterItemsInMenuGroup', () => {
+    const { mapAndFilterItemsInMenuGroup } = EXPORTED_FOR_UNIT_TESTING
+    const ACTION_1 = 'action1'
+    const actionViewProp: ActionViewProps = {
+      action: ACTION_1,
+      icon: 'article',
+      textSx: { color: 'blue' },
+      iconSx: { color: 'green' },
+    }
+    const onClickFn = jest.fn()
+    const actionConfiguration: ActionConfiguration = {
+      text: 'action 1 text',
+      visible: true,
+      disabled: false,
+      onClick: onClickFn,
+      href: 'my-href',
+      tooltipText: 'action 1 tooltip',
+    }
+
+    it('Properly maps a menu group to a DropdownMenuItem array', () => {
+      const group: ActionViewProps[] = [actionViewProp]
+      const actionConfigurationMap: ActionConfigurationMap = {
+        [ACTION_1]: actionConfiguration,
+      }
+      // Call under test
+      const result = mapAndFilterItemsInMenuGroup(group, actionConfigurationMap)
+
+      expect(result).toEqual([
+        {
+          text: 'action 1 text',
+          icon: 'article',
+          onClick: onClickFn,
+          href: 'my-href',
+          tooltipText: 'action 1 tooltip',
+          disabled: false,
+          textSx: { color: 'blue' },
+          iconSx: { color: 'green' },
+        },
+      ])
+    })
+    it('Removes items that do not have a configuration', () => {
+      const group: ActionViewProps[] = [actionViewProp]
+      const actionConfigurationMap: ActionConfigurationMap = {
+        // Action 1 is not configured.
+      }
+      // Call under test
+      const result = mapAndFilterItemsInMenuGroup(group, actionConfigurationMap)
+
+      expect(result).toEqual([])
+    })
+    it('Removes items that are not visible', () => {
+      const group: ActionViewProps[] = [actionViewProp]
+      const actionConfigurationMap: ActionConfigurationMap = {
+        [ACTION_1]: { ...actionConfiguration, visible: false },
+      }
+      // Call under test
+      const result = mapAndFilterItemsInMenuGroup(group, actionConfigurationMap)
+
+      expect(result).toEqual([])
+    })
+  })
+
+  describe('mapAndFilterMenuGroups', () => {
+    const { mapAndFilterMenuGroups } = EXPORTED_FOR_UNIT_TESTING
+
+    const ACTION_1 = 'action1'
+    const ACTION_2 = 'action2'
+    const actionViewProp1: ActionViewProps = {
+      action: ACTION_1,
+      icon: 'article',
+      textSx: { color: 'blue' },
+      iconSx: { color: 'green' },
+    }
+    const actionViewProp2: ActionViewProps = {
+      action: ACTION_2,
+      icon: 'edit',
+      textSx: { color: 'red' },
+      iconSx: { color: 'purple' },
+    }
+    const onClickFn = jest.fn()
+    const actionConfiguration1: ActionConfiguration = {
+      text: 'action 1 text',
+      visible: true,
+      disabled: false,
+      onClick: onClickFn,
+      href: 'my-href',
+      tooltipText: 'action 1 tooltip',
+    }
+    const actionConfiguration2: ActionConfiguration = {
+      text: 'action 2 text',
+      visible: true,
+      disabled: true,
+      onClick: onClickFn,
+      href: 'another-href',
+      tooltipText: 'action 2 tooltip',
+    }
+    it('Properly maps an array of menu groups to an array of DropdownMenuItem groups', () => {
+      const menuActions = [[actionViewProp1], [actionViewProp2]]
+
+      const actionConfigurationMap: ActionConfigurationMap = {
+        [ACTION_1]: actionConfiguration1,
+        [ACTION_2]: actionConfiguration2,
+      }
+
+      // Call under test
+      const result = mapAndFilterMenuGroups(menuActions, actionConfigurationMap)
+
+      expect(result).toEqual([
+        [
+          {
+            text: 'action 1 text',
+            icon: 'article',
+            onClick: onClickFn,
+            href: 'my-href',
+            tooltipText: 'action 1 tooltip',
+            disabled: false,
+            textSx: { color: 'blue' },
+            iconSx: { color: 'green' },
+          },
+        ],
+        [
+          {
+            text: 'action 2 text',
+            icon: 'edit',
+            onClick: onClickFn,
+            href: 'another-href',
+            tooltipText: 'action 2 tooltip',
+            disabled: true,
+            textSx: { color: 'red' },
+            iconSx: { color: 'purple' },
+          },
+        ],
+      ])
+    })
+    it('Removes groups that have no configured/visible items', () => {
+      const menuActions = [[actionViewProp1], [actionViewProp2]]
+
+      const actionConfigurationMap: ActionConfigurationMap = {
+        [ACTION_1]: { ...actionConfiguration1, visible: false },
+        // Action 2 is not configured
+      }
+
+      // Call under test
+      const result = mapAndFilterMenuGroups(menuActions, actionConfigurationMap)
+
+      // Note that the empty groups were removed, i.e. the result is NOT [[], []]
+      expect(result).toEqual([])
+    })
+  })
+})
---
+++ src/lib/containers/entity/page/action_menu/EntityActionMenu.tsx
@@ -0,0 +1,234 @@
+import React from 'react'
+import { SxProps } from '@mui/material'
+import { ComplexMenu, IconButtonConfiguration } from '../../../menu/ComplexMenu'
+import IconSvg, { Icon } from '../../../IconSvg'
+import { DropdownMenuItem, DropdownMenuProps } from '../../../menu/DropdownMenu'
+
+// Represents the two types of dropdown menus that will be displayed on the entity page
+type EntityActionMenuDropdownMenuType = 'DOWNLOAD' | 'PRIMARY'
+
+type EntityActionMenuDropdownMenuConfiguration = {
+  visible: boolean
+  tooltipText?: string
+  disabled?: boolean
+}
+
+export type ActionConfiguration = {
+  visible: boolean
+  text: string
+  disabled?: boolean
+  tooltipText?: string
+  onClick?: (e: React.MouseEvent) => void
+  href?: string
+}
+
+export type ActionConfigurationMap = Record<string, ActionConfiguration>
+
+export type MenuConfigurationMap = Record<
+  EntityActionMenuDropdownMenuType,
+  EntityActionMenuDropdownMenuConfiguration
+>
+
+export type ActionViewProps = {
+  action: string
+  icon?: Icon
+  textSx?: SxProps
+  iconSx?: SxProps
+}
+
+export type EntityActionMenuLayout = {
+  buttonActions: ActionViewProps[]
+  downloadMenuActions: ActionViewProps[][]
+  primaryMenuActions: ActionViewProps[][]
+  primaryMenuText: string
+  primaryMenuEndIcon: Icon
+  menuButtonSx?: SxProps
+}
+
+export type EntityActionMenuProps = {
+  /* Maps a unique action string to a dynamic configuration for that particular action */
+  actionConfiguration: ActionConfigurationMap
+  /* Maps each dropdown menu to a dynamic configuration for that particular menu */
+  menuConfiguration: MenuConfigurationMap
+  /* Defines the layout of the actions. Typically depends on just the entity type and area. */
+  layout: EntityActionMenuLayout
+}
+
+/*
+ * All visible actions should be specified in the layout, but this is not guaranteed at compile time.
+ * Find all visible actions in the action configuration that are not in the layout.
+ */
+function getUnmappedActions(
+  actionConfiguration: ActionConfigurationMap,
+  layout: EntityActionMenuLayout,
+): ActionConfigurationMap {
+  return Object.fromEntries(
+    Object.entries(actionConfiguration).filter(([action, config]) => {
+      // We don't care about actions that are not configured or should not be visible
+      if (!config || !config.visible) {
+        return false
+      }
+
+      const allLayoutActions: ActionViewProps[] = [
+        ...layout.buttonActions,
+        ...layout.primaryMenuActions.flat(),
+        ...layout.downloadMenuActions.flat(),
+      ]
+      // Action is unmapped if it's visible and the layout doesn't have it
+      return !allLayoutActions.some(
+        layoutAction => layoutAction.action === action,
+      )
+    }),
+  )
+}
+
+/**
+ * Given a group of dropdown menu items and an action configuration,
+ * return a list of dropdown menu items with non-visible and non-configured actions removed.
+ * @param group
+ * @param actionConfiguration
+ */
+function mapAndFilterItemsInMenuGroup(
+  group: ActionViewProps[],
+  actionConfiguration: ActionConfigurationMap,
+): DropdownMenuItem[] {
+  return group.reduce(
+    (itemAcc: DropdownMenuItem[], actionViewProps: ActionViewProps) => {
+      const configForAction = actionConfiguration[actionViewProps.action]
+      // Only show the item if it's configured + visible
+      if (configForAction && configForAction.visible) {
+        itemAcc.push({
+          text: configForAction.text ?? actionViewProps.action,
+          onClick: configForAction.onClick,
+          href: configForAction.href,
+          tooltipText: configForAction.tooltipText,
+          disabled: configForAction.disabled,
+          icon: actionViewProps.icon,
+          textSx: actionViewProps.textSx,
+          iconSx: actionViewProps.iconSx,
+        })
+      }
+      return itemAcc
+    },
+    [],
+  )
+}
+
+/**
+ * Given a list of groups of dropdown menu actions, returns a list of dropdown menu item
+ * groups where non-visible actions have been removed, and groups with no visible actions
+ * have been removed.
+ * @param menuActions
+ * @param actionConfiguration
+ */
+function mapAndFilterMenuGroups(
+  menuActions: ActionViewProps[][],
+  actionConfiguration: ActionConfigurationMap,
+): DropdownMenuItem[][] {
+  return menuActions.reduce(
+    (groupAcc: DropdownMenuItem[][], group: ActionViewProps[]) => {
+      const itemGroup = mapAndFilterItemsInMenuGroup(group, actionConfiguration)
+      // Only show the group if it contains visible items
+      if (itemGroup.length > 0) {
+        groupAcc.push(itemGroup)
+      }
+      return groupAcc
+    },
+    [],
+  )
+}
+
+/**
+ * The EntityActionMenu renders a menu that displays the actions that can be invoked on an Entity page.
+ */
+export default function EntityActionMenu(props: EntityActionMenuProps) {
+  const { actionConfiguration, menuConfiguration, layout } = props
+
+  /*
+   * All actions should be specified in the layout, but this is not guaranteed at compile time.
+   * Find all visible actions in the action configuration that are not in the layout.
+   */
+  const unmappedActions = getUnmappedActions(actionConfiguration, layout)
+  // Warn if unmapped actions were found and push them on to the layout.
+  if (Object.entries(unmappedActions).length > 0) {
+    console.warn(
+      'Actions are visible but have not been configured in the layout:',
+      Object.entries(unmappedActions).map(entry => entry[0]),
+    )
+    // Modify the layout: put the unmapped actions in their own group at the top of the menu
+    layout.primaryMenuActions.unshift(
+      Object.entries(unmappedActions).map(
+        (entry): ActionViewProps => ({
+          action: entry[0],
+        }),
+      ),
+    )
+  }
+
+  // Map button actions to an IconButtonConfiguration and omit non-visible actions
+  const iconButtonConfigs: IconButtonConfiguration[] =
+    layout.buttonActions.reduce(
+      (acc: IconButtonConfiguration[], buttonViewProps: ActionViewProps) => {
+        const configForAction = actionConfiguration[buttonViewProps.action]
+        if (configForAction && configForAction.visible) {
+          let onClick = configForAction.onClick
+          if (onClick == null) {
+            console.warn(`No handler registered for ${buttonViewProps.action}`)
+            onClick = () => {
+              console.warn(
+                `No handler registered for ${buttonViewProps.action}`,
+              )
+            }
+          }
+          acc.push({
+            icon: buttonViewProps.icon as Icon,
+            onClick: onClick,
+            tooltipText: configForAction.text,
+          })
+        }
+        return acc
+      },
+      [],
+    )
+
+  const downloadMenuConfig: DropdownMenuProps = {
+    dropdownButtonText: 'Download Options',
+    convertSingleItemToButton: false,
+    renderMenuIfNoItems: false,
+    buttonTooltip: menuConfiguration.DOWNLOAD.tooltipText,
+    buttonProps: {
+      disabled: menuConfiguration.DOWNLOAD.disabled,
+      endIcon: <IconSvg icon={'download'} wrap={false} />,
+    },
+    items: mapAndFilterMenuGroups(
+      layout.downloadMenuActions,
+      actionConfiguration,
+    ),
+  }
+
+  const primaryMenuConfig: DropdownMenuProps = {
+    dropdownButtonText: layout.primaryMenuText,
+    convertSingleItemToButton: true,
+    renderMenuIfNoItems: false,
+    buttonProps: {
+      endIcon: <IconSvg icon={layout.primaryMenuEndIcon} wrap={false} />,
+    },
+    items: mapAndFilterMenuGroups(
+      layout.primaryMenuActions,
+      actionConfiguration,
+    ),
+  }
+
+  return (
+    <ComplexMenu
+      iconButtons={iconButtonConfigs}
+      dropdownMenus={[downloadMenuConfig, primaryMenuConfig]}
+    />
+  )
+}
+
+export const EXPORTED_FOR_UNIT_TESTING = {
+  getUnmappedActions,
+  mapAndFilterItemsInMenuGroup,
+  mapAndFilterMenuGroups,
+}
